f7e118dd90028b654d9f4e347fbb6f458d56b7d7
boratln <boratulan123@gmail.com>
2024-10-27 18:39:50 +0300

    CQRS GetAboutQueryResult

---
+++ Blogizm.Application/Features/CQRS/Results/AboutResults/GetAboutQueryResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Results.AboutResults
+{
+    public class GetAboutQueryResult
+    {
+        public int AboutId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
