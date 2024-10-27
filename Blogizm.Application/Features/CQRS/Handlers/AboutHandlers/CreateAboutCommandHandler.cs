a0f694854b47b24740175081201255f2b6c795f9
boratln <boratulan123@gmail.com>
2024-10-27 18:12:58 +0300

    CQRS CreateAboutCommandHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/AboutHandlers/CreateAboutCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Blogizm.Application.Features.CQRS.Commands;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class CreateAboutCommandHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public CreateAboutCommandHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(CreateAboutCommand command)
+        {
+            await _repository.Create(new About
+            {
+                Title = command.Title,
+                Description = command.Description,
+
+            });
+        }
+    }
+}
