585114e3daf7e65b0370f16759696088bf4cd9b9
boratln <boratulan123@gmail.com>
2024-10-27 18:13:41 +0300

    CQRS UpdateAboutCommandHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/AboutHandlers/UpdateAboutCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Blogizm.Application.Features.CQRS.Commands;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class UpdateAboutCommandHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public UpdateAboutCommandHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(UpdateAboutCommand command)
+        {
+            var value = await _repository.GetById(command.AboutId);
+            value.Description = command.Description;
+            value.Title = command.Title;
+            await _repository.Update(value);
+        }
+    }
+}
