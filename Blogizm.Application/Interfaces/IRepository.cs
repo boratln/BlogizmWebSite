4e092801ec241a563edc2d6e151b6a72ace80bfa
boratln <boratulan123@gmail.com>
2024-10-27 18:39:59 +0300

    IRepository

---
+++ Blogizm.Application/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAll();
+        Task<T> GetById(int id);
+        Task Create(T entity);
+        Task Update(T entity);
+        Task Remove(T entity);
+    }
+}
