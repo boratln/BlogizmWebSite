dee963865c139f377f023e964355018459f8a5c0
boratln <boratulan123@gmail.com>
2024-10-27 18:41:40 +0300

    Repository

---
+++ Blogizm.Persistence/Repositories/Repository.cs
@@ -0,0 +1,49 @@
+﻿using Blogizm.Application.Interfaces;
+using Blogizm.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Persistence.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly BlogContext _blogContext;
+
+        public Repository(BlogContext blogContext)
+        {
+            _blogContext = blogContext;
+        }
+
+        public async Task Create(T entity)
+        {
+            _blogContext.Set<T>().Add(entity);
+            await _blogContext.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> GetAll()
+        {
+            return await _blogContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await _blogContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task Remove(T entity)
+        {
+            _blogContext.Set<T>().Remove(entity);
+            await _blogContext.SaveChangesAsync();
+        }
+
+        public async Task Update(T entity)
+        {
+            _blogContext.Set<T>().Update(entity);
+            await _blogContext.SaveChangesAsync();
+        }
+    }
+}
