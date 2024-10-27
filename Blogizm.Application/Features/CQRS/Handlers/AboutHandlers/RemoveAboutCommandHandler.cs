af93143f007d47b139379f64017fe5d6887ace5a
boratln <boratulan123@gmail.com>
2024-10-27 18:13:30 +0300

    CQRS RemoveAboutCommandHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/AboutHandlers/RemoveAboutCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Blogizm.Application.Features.CQRS.Commands;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class RemoveAboutCommandHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public RemoveAboutCommandHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(RemoveAboutCommand command)
+        {
+            var value=await _repository.GetById(command.Id);
+            await _repository.Remove(value);
+        } 
+    }
+}
