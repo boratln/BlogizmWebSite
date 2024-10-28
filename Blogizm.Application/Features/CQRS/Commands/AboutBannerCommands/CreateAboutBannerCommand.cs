3b42b2437dee24d1a8e9bae69563bfdb05cf32c3
boratln <boratulan123@gmail.com>
2024-10-28 23:10:10 +0300

    CQRS CreateAboutBannerCommand

---
+++ Blogizm.Application/Features/CQRS/Commands/AboutBannerCommands/CreateAboutBannerCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Commands.AboutBannerCommands
+{
+    public class CreateAboutBannerCommand
+    {
+        public string BannerTitle1 { get; set; }
+        public string BannerDesc1 { get; set; }
+        public string BannerImg1 { get; set; }
+        public string BannerTitle2 { get; set; }
+        public string BannerDesc2 { get; set; }
+        public string BannerImg2 { get; set; }
+    }
+}
