28cb04f9a9d6c21d75d1aa4bb90096ccad510620
boratln <boratulan123@gmail.com>
2024-10-28 23:10:24 +0300

    CQRS RemoveAboutBannerCommand

---
+++ Blogizm.Application/Features/CQRS/Commands/AboutBannerCommands/RemoveAboutBannerCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Commands.AboutBannerCommands
+{
+    public class RemoveAboutBannerCommand
+    {
+        public int Id { get; set; }
+
+        public RemoveAboutBannerCommand(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
