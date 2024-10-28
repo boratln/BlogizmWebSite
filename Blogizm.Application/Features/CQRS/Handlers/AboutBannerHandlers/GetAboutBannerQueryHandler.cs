ee549684687ab83e441b50092b4109fb23d09cdc
boratln <boratulan123@gmail.com>
2024-10-28 23:12:18 +0300

    CQRS GetAboutBannerQueryHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/AboutBannerHandlers/GetAboutBannerQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using Blogizm.Application.Features.CQRS.Results.AboutBannerResults;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.AboutBannerHandlers
+{
+    public class GetAboutBannerQueryHandler
+    {
+        private readonly IRepository<AboutBanner> _repository;
+
+        public GetAboutBannerQueryHandler(IRepository<AboutBanner> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<List<GetAboutBannerQueryResult>> Handle()
+        {
+            var values=await _repository.GetAll();
+            return values.Select(x => new GetAboutBannerQueryResult
+            {
+                AboutBannerId = x.AboutBannerId,
+                BannerDesc1 = x.BannerDesc1,
+                BannerDesc2 = x.BannerDesc2,
+                BannerImg1 = x.BannerImg1,
+                BannerImg2 = x.BannerImg2,
+                BannerTitle1 = x.BannerTitle1,
+                BannerTitle2 = x.BannerTitle2,
+            }).ToList();
+        }
+    }
+}
