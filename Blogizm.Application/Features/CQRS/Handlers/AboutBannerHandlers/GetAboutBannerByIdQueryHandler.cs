4fdfcd2e4362c30de99884acc72c09eb690d812e
boratln <boratulan123@gmail.com>
2024-10-28 23:12:07 +0300

    CQRS GetAboutBannerByIdQueryHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/AboutBannerHandlers/GetAboutBannerByIdQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using Blogizm.Application.Features.CQRS.Queries.AboutBannerQueries;
+using Blogizm.Application.Features.CQRS.Results.AboutBannerResults;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.AboutBannerHandlers
+{
+    public class GetAboutBannerByIdQueryHandler
+    {
+        private readonly IRepository<AboutBanner> _repository;
+
+        public GetAboutBannerByIdQueryHandler(IRepository<AboutBanner> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<GetAboutBannerByIdQueryResult> Handle(GetAboutBannerByIdQuery query)
+        {
+            var value = await _repository.GetById(query.Id);
+            return new GetAboutBannerByIdQueryResult
+            {
+                AboutBannerId = query.Id,
+                BannerDesc1 = value.BannerDesc1,
+                BannerDesc2 = value.BannerDesc2,
+                BannerImg1 = value.BannerImg1,
+                BannerImg2 = value.BannerImg2,
+                BannerTitle1 = value.BannerTitle1,
+                BannerTitle2 = value.BannerTitle2,
+
+            };
+        }
+    }
+}
