400b2526677836eecfe8501f0533e6ff1fd1117a
boratln <boratulan123@gmail.com>
2024-10-28 23:11:38 +0300

    CQRS About Commands Moving Folder

---
+++ Blogizm.Application/Features/CQRS/Commands/AboutCommands/CreateAboutCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Commands.AboutCommands
+{
+    public class CreateAboutCommand
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
