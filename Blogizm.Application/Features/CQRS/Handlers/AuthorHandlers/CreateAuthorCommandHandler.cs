310f93a605da4f7a2b9a72b0bc2fe49830da7551
boratln <boratulan123@gmail.com>
2024-10-29 22:18:55 +0300

    CQRS CreateAuthorCommandHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/AuthorHandlers/CreateAuthorCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Blogizm.Application.Features.CQRS.Commands.AboutCommands;
+using Blogizm.Application.Features.CQRS.Commands.AuthorCommands;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class CreateAuthorCommandHandler
+    {
+        private readonly IRepository<Author> _repository;
+
+        public CreateAuthorCommandHandler(IRepository<Author> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(CreateAuthorCommand command)
+        {
+            await _repository.Create(new Author
+            {
+              AuthorImage=command.AuthorImage,
+              Description=command.Description,
+              Name=command.Name,
+              Surname=command.Surname,
+
+            });
+        }
+    }
+}
