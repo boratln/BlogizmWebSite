52f6fd732ba81b0a839e632f715c3438a2eca925
boratln <boratulan123@gmail.com>
2024-10-29 22:18:46 +0300

    CQRS UpdateClientCommand

---
+++ Blogizm.Application/Features/CQRS/Commands/ClientCommands/UpdateClientCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Commands.ClientCommands
+{
+    public class UpdateClientCommand
+    {
+        public int ClientId { get; set; }
+        public string ClientName { get; set; }
+        public string ClientDesc { get; set; }
+        public string ClientImage { get; set; }
+    }
+}
