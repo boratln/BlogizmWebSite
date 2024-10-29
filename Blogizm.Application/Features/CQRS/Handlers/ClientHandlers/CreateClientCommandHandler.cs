7267d96fdff71b47d7c27d60bcd8991da48d00fb
boratln <boratulan123@gmail.com>
2024-10-29 22:25:11 +0300

    CQRS CreateClientCommandHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/ClientHandlers/CreateClientCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Blogizm.Application.Features.CQRS.Commands.AboutCommands;
+using Blogizm.Application.Features.CQRS.Commands.AuthorCommands;
+using Blogizm.Application.Features.CQRS.Commands.ClientCommands;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class CreateClientCommandHandler
+    {
+        private readonly IRepository<Client> _repository;
+
+        public CreateClientCommandHandler(IRepository<Client> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(CreateClientCommand command)
+        {
+            await _repository.Create(new Client
+            {
+             ClientDesc = command.ClientDesc,
+             ClientImage= command.ClientImage,
+             ClientName= command.ClientName,
+
+            });
+        }
+    }
+}
