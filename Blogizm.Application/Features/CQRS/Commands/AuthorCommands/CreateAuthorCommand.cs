11e08fe043fadfea58ac86c34c4202a895d7c99b
boratln <boratulan123@gmail.com>
2024-10-29 22:17:31 +0300

    CQRS CreateAuthorCommand

---
+++ Blogizm.Application/Features/CQRS/Commands/AuthorCommands/CreateAuthorCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Commands.AuthorCommands
+{
+    public class CreateAuthorCommand
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Description { get; set; }
+        public string AuthorImage { get; set; }
+    }
+}
