45ec841cc2e07679ff6507052dc7d8b43ee32689
boratln <boratulan123@gmail.com>
2024-10-29 22:28:09 +0300

    Blog Table Add Column AuthorId  Migrations

---
+++ Blogizm.Persistence/Migrations/20241029175558_addcolumnBlogTable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blogizm.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class addcolumnBlogTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            migrationBuilder.AddColumn<int>(
+           name: "AuthorId",
+           table: "Blogs",
+           type: "int",
+           nullable: false,
+           defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_Authors_AuthorId",
+                table: "Blogs",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "AuthorId",
+                onDelete: ReferentialAction.Cascade);
+
+        }
+
+
+    }
+}
