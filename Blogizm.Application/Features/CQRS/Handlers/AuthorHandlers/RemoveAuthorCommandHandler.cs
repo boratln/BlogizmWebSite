c2561a898bcdc6d137821649b38bec675e8c7b66
boratln <boratulan123@gmail.com>
2024-10-29 22:19:27 +0300

    CQRS RemoveAuthorCommandHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/AuthorHandlers/RemoveAuthorCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Blogizm.Application.Features.CQRS.Commands.AboutCommands;
+using Blogizm.Application.Features.CQRS.Commands.AuthorCommands;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class RemoveAuthorCommandHandler
+    {
+        private readonly IRepository<Author> _repository;
+
+        public RemoveAuthorCommandHandler(IRepository<Author> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(RemoveAuthorCommand command)
+        {
+            var value=await _repository.GetById(command.Id);
+            await _repository.Remove(value);
+        } 
+    }
+}
