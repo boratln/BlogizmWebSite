226da8cf739263db298b4776490a964394c2ff16
boratln <boratulan123@gmail.com>
2024-10-29 22:17:44 +0300

    CQRS RemoveAuthorCommand

---
+++ Blogizm.Application/Features/CQRS/Commands/AuthorCommands/RemoveAuthorCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Commands.AuthorCommands
+{
+    public class RemoveAuthorCommand
+    {
+        public int Id { get; set; }
+
+        public RemoveAuthorCommand(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
