43cbd960a6b1e6dc00edb1acdaab6267aa39113d
boratln <boratulan123@gmail.com>
2024-10-29 22:19:17 +0300

    CQRS GetAuthorQueryHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/AuthorHandlers/GetAuthorQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using Blogizm.Application.Features.CQRS.Results.AboutResults;
+using Blogizm.Application.Features.CQRS.Results.AuthorResults;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class GetAuthorQueryHandler
+    {
+        private readonly IRepository<Author> _repository;
+
+        public GetAuthorQueryHandler(IRepository<Author> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<List<GetAuthorQueryResult>> Handle()
+        {
+            var values=await _repository.GetAll();
+            return values.Select(x => new GetAuthorQueryResult
+            {
+               Surname = x.Surname,
+               Name = x.Name,
+               Description = x.Description,
+               AuthorImage = x.AuthorImage,
+               AuthorId = x.AuthorId
+            }).ToList();
+        }
+    }
+}
