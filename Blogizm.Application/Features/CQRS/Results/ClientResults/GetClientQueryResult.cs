2a3cbc299564cc146b994294d0c8eab50cf537b0
boratln <boratulan123@gmail.com>
2024-10-29 22:27:04 +0300

    CQRS GetClientQueryResult

---
+++ Blogizm.Application/Features/CQRS/Results/ClientResults/GetClientQueryResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Results.AuthorResults
+{
+    public class GetClientQueryResult
+    {
+        public int ClientId { get; set; }
+        public string ClientName { get; set; }
+        public string ClientDesc { get; set; }
+        public string ClientImage { get; set; }
+    }
+}
