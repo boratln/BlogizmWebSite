3ba313444d859ce5848295e611bddfc87c07498b
boratln <boratulan123@gmail.com>
2024-10-29 22:18:30 +0300

    CQRS CreateClientCommand

---
+++ Blogizm.Application/Features/CQRS/Commands/ClientCommands/CreateClientCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Commands.ClientCommands
+{
+    public class CreateClientCommand
+    {
+        public string ClientName { get; set; }
+        public string ClientDesc { get; set; }
+        public string ClientImage { get; set; }
+    }
+}
