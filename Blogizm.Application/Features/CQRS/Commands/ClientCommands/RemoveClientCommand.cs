73a63591303d5175ad8cddfdb05e82ad169108ab
boratln <boratulan123@gmail.com>
2024-10-29 22:18:37 +0300

    CQRS RemoveClientCommand

---
+++ Blogizm.Application/Features/CQRS/Commands/ClientCommands/RemoveClientCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Commands.ClientCommands
+{
+    public class RemoveClientCommand
+    {
+        public int Id { get; set; }
+
+        public RemoveClientCommand(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
