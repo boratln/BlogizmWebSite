3acea580f11600638638147fb36f42f52161edcd
boratln <boratulan123@gmail.com>
2024-11-18 23:18:23 +0300

    CQRS GetSocialMediaQueryHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/SocialMediaHandlers/GetSocialMediaQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using Blogizm.Application.Features.CQRS.Results.SocialMediaResults;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.SocialMediaHandlers
+{
+    public class GetSocialMediaQueryHandler
+    {
+        private readonly IRepository<SocialMedia> _repository;
+
+        public GetSocialMediaQueryHandler(IRepository<SocialMedia> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<List<GetSocialMediaQueryResult>> Handle()
+        {
+            var values = await _repository.GetAll();
+            return values.Select(x => new GetSocialMediaQueryResult
+            {
+                Icon = x.Icon,
+                SocialMediaId = x.SocialMediaId,
+                Title = x.Title,
+                URL = x.URL,
+            }).ToList();
+        }
+    }
+}
