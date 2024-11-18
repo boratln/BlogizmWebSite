927c19ef9726c54624d41d4c6054c75ee0375e0d
boratln <boratulan123@gmail.com>
2024-11-18 23:16:01 +0300

    CQRS RemoveTagCommand

---
+++ Blogizm.Application/Features/CQRS/Commands/TagCommands/RemoveTagCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Commands.TagCommands
+{
+    public class RemoveTagCommand
+    {
+        public int Id { get; set; }
+
+        public RemoveTagCommand(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
