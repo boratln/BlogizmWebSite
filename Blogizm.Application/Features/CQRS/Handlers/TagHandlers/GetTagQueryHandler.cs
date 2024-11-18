a2cf6776111190523fb8357b3a4c303f48f211c9
boratln <boratulan123@gmail.com>
2024-11-18 23:33:10 +0300

    CQRS GetTagQueryHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/TagHandlers/GetTagQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using Blogizm.Application.Features.CQRS.Results.TagResults;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.TagHandlers
+{
+    public class GetTagQueryHandler
+    {
+        private readonly IRepository<Tag> _tagRepository;
+
+        public GetTagQueryHandler(IRepository<Tag> tagRepository)
+        {
+            _tagRepository = tagRepository;
+        }
+        public async Task<List<GetTagQueryResult>> Handle()
+        {
+            var values=await _tagRepository.GetAll();
+            return values.Select(x => new GetTagQueryResult
+            {
+                TagId = x.TagId,
+                Title = x.Title
+            }).ToList();
+        }
+    }
+}
