38bed8040c7c4f3f02d34a8e9b9ecda103f58931
boratln <boratulan123@gmail.com>
2024-11-18 23:18:52 +0300

    Contact,Social Media ,Tag Handlers Dependency Injection

--- Blogizm.WebApi/Program.cs
+++ Blogizm.WebApi/Program.cs
@@ -1,16 +1,15 @@
 using Blogizm.Application.Features.CQRS.Handlers.AboutBannerHandlers;
 using Blogizm.Application.Features.CQRS.Handlers.AboutHandlers;
+using Blogizm.Application.Features.CQRS.Handlers.ContactAddressHandlers;
+using Blogizm.Application.Features.CQRS.Handlers.SocialMediaHandlers;
+using Blogizm.Application.Features.CQRS.Handlers.TagHandlers;
 using Blogizm.Application.Interfaces;
 using Blogizm.Persistence.Context;
 using Blogizm.Persistence.Repositories;
-
 var builder = WebApplication.CreateBuilder(args);
-
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddScoped<BlogContext>();
 //About
 builder.Services.AddScoped<GetAboutQueryHandler>();
@@ -36,6 +35,25 @@ builder.Services.AddScoped<GetClientByIdQueryHandler>();
 builder.Services.AddScoped<RemoveClientCommandHandler>();
 builder.Services.AddScoped<CreateClientCommandHandler>();
 builder.Services.AddScoped<UpdateClientCommandHandler>();
+
+//ContactAddress
+builder.Services.AddScoped<GetContactAddressQueryHandler>();
+builder.Services.AddScoped<GetContactAddressByIdQueryHandler>();
+builder.Services.AddScoped<RemoveContactAddressCommandHandlerr>();
+builder.Services.AddScoped<CreateContactAddressCommandHandler>();
+builder.Services.AddScoped<UpdateContactAddressCommandHandler>();
+//Social Media
+builder.Services.AddScoped<GetSocialMediaQueryHandler>();
+builder.Services.AddScoped<GetSocialMediaByIdQueryHandler>();
+builder.Services.AddScoped<RemoveSocialMediaCommandHandler>();
+builder.Services.AddScoped<CreateSocialMediaCommandHandler>();
+builder.Services.AddScoped<UpdateSocialMediaCommandHandler>();
+//Tag
+builder.Services.AddScoped<GetTagQueryHandler>();
+builder.Services.AddScoped<GetTagByIdQueryHandler>();
+builder.Services.AddScoped<RemoveTagCommandHandler>();
+builder.Services.AddScoped<CreateTagCommandHandler>();
+builder.Services.AddScoped<UpdateTagCommandHandler>();
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 var app = builder.Build();
 
