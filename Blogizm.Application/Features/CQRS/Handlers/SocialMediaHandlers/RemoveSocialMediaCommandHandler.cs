7d3f4369d1e9cb972cc71e7163131055ce83ab2a
boratln <boratulan123@gmail.com>
2024-11-18 23:22:17 +0300

    CQRS RemoveSocialMediaCommandHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/SocialMediaHandlers/RemoveSocialMediaCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Blogizm.Application.Features.CQRS.Commands.SocialMediaCommands;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.SocialMediaHandlers
+{
+    public class RemoveSocialMediaCommandHandler
+    {
+        private readonly IRepository<SocialMedia> _repository;
+
+        public RemoveSocialMediaCommandHandler(IRepository<SocialMedia> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(RemoveSocialMediaCommand command)
+        {
+            var value = await _repository.GetById(command.Id);
+            await _repository.Remove(value);
+        }
+    }
+}
