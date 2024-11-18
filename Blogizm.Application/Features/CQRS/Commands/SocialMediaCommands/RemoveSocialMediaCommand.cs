5477171b6c635ad5ac92939b62005a269c83f540
boratln <boratulan123@gmail.com>
2024-11-18 23:15:35 +0300

    CQRS RemoveSocialMediaCommand

---
+++ Blogizm.Application/Features/CQRS/Commands/SocialMediaCommands/RemoveSocialMediaCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Commands.SocialMediaCommands
+{
+    public class RemoveSocialMediaCommand
+    {
+        public int Id { get; set; }
+
+        public RemoveSocialMediaCommand(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
