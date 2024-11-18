ebfb01854c388ed733cffc2e1d43b9dfedd80008
boratln <boratulan123@gmail.com>
2024-11-18 23:33:18 +0300

    CQRS GetTagByIdQuery

---
+++ Blogizm.Application/Features/CQRS/Queries/TagQueries/GetTagByIdQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Queries.TagQueries
+{
+    public class GetTagByIdQuery
+    {
+        public int Id { get; set; }
+
+        public GetTagByIdQuery(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
