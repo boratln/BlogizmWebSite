176d1c1a25504c0c71e2c0983f6cc18067772d3d
boratln <boratulan123@gmail.com>
2024-11-18 23:34:04 +0300

    CQRS GetSocialMediaByIdQuery

---
+++ Blogizm.Application/Features/CQRS/Queries/SocialMediaQueries/GetSocialMediaByIdQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Queries.SocialMediaQueries
+{
+    public class GetSocialMediaByIdQuery
+    {
+        public int Id { get; set; }
+
+        public GetSocialMediaByIdQuery(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
