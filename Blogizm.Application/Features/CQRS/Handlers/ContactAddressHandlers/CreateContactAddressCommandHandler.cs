5305f520802e77ea89beb987f310f2455dc3699f
boratln <boratulan123@gmail.com>
2024-11-18 23:16:25 +0300

    CQRS CreateContactAddressCommandHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/ContactAddressHandlers/CreateContactAddressCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Blogizm.Application.Features.CQRS.Commands.ContactAddressCommands;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.ContactAddressHandlers
+{
+    public class CreateContactAddressCommandHandler
+    {
+        private readonly IRepository<ContactAddress> _repository;
+
+        public CreateContactAddressCommandHandler(IRepository<ContactAddress> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(CreateContactAddressCommand command)
+        {
+            await _repository.Create(new ContactAddress
+            {
+                Icon = command.Icon,
+                Title = command.Title,
+                URL = command.URL,
+            });
+        }
+    }
+}
