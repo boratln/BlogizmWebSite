a753a95c713a001a46c0aaec4ef6a65ede83b614
boratln <boratulan123@gmail.com>
2024-11-18 23:17:45 +0300

    CQRS UpdateContactAddressCommandHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/ContactAddressHandlers/UpdateContactAddressCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Blogizm.Application.Features.CQRS.Commands.ContactAddressCommands;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.ContactAddressHandlers
+{
+    public class UpdateContactAddressCommandHandler
+    {
+        private readonly IRepository<ContactAddress> _repository;
+
+        public UpdateContactAddressCommandHandler(IRepository<ContactAddress> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(UpdateContactAddressCommand command)
+        {
+            var value = await _repository.GetById(command.ContactAddressId);
+            value.Title = command.Title;
+            value.URL= command.URL;
+            value.Icon= command.Icon;
+            await _repository.Update(value);
+        }
+    }
+}
