762338a66ba3e3de620a80dc3160dee4a1c754bd
boratln <boratulan123@gmail.com>
2024-11-18 23:14:47 +0300

    CQRS CreateContactAddressCommand

---
+++ Blogizm.Application/Features/CQRS/Commands/ContactAddressCommands/CreateContactAddressCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Commands.ContactAddressCommands
+{
+    public class CreateContactAddressCommand
+    {
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public string URL { get; set; }
+    }
+}
