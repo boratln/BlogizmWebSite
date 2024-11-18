d719002b0706c1a94b63a9c8c1c8f2af077e329b
boratln <boratulan123@gmail.com>
2024-11-18 23:22:27 +0300

    CQRS UpdateSocialMediaCommandHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/SocialMediaHandlers/UpdateSocialMediaCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Blogizm.Application.Features.CQRS.Commands.SocialMediaCommands;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.SocialMediaHandlers
+{
+    public class UpdateSocialMediaCommandHandler
+    {
+        private readonly IRepository<SocialMedia> _repository;
+
+        public UpdateSocialMediaCommandHandler(IRepository<SocialMedia> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(UpdateSocialMediaCommand command)
+        {
+            var value = await _repository.GetById(command.SocialMediaId);
+            value.Title = command.Title;
+            value.Icon=command.Icon;
+            await _repository.Update(value);
+        }
+    }
+}
