8b136389aa61a761dda790c94e31d6fe021e6d65
boratln <boratulan123@gmail.com>
2024-11-18 23:34:15 +0300

    CQRS GetContactAddressByIdQuery

---
+++ Blogizm.Application/Features/CQRS/Queries/ContactAddressQueries/GetContactAddressByIdQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Queries.ContactAddressQueries
+{
+    public class GetContactAddressByIdQuery
+    {
+        public int Id { get; set; }
+
+        public GetContactAddressByIdQuery(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
