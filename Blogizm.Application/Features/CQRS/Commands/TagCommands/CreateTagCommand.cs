e654e7cf876f8e1169734a6ebf5581c347981689
boratln <boratulan123@gmail.com>
2024-11-18 23:15:54 +0300

    CQRS CreateTagCommand

---
+++ Blogizm.Application/Features/CQRS/Commands/TagCommands/CreateTagCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Commands.TagCommands
+{
+    public class CreateTagCommand
+    {
+        public string Title { get; set; }
+    }
+}
