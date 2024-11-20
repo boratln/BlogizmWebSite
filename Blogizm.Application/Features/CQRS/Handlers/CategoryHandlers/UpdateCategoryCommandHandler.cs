2587897e4942c918e6844f0e67dbe3a48cfd2997
boratln <boratulan123@gmail.com>
2024-11-20 23:18:34 +0300

    CQRS UpdateCategoryCommandHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/CategoryHandlers/UpdateCategoryCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Blogizm.Application.Features.CQRS.Commands.CategoryCommands;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.CategoryHandlers
+{
+    public class UpdateCategoryCommandHandler
+    {
+        private readonly IRepository<Category> _categoryRepository;
+
+        public UpdateCategoryCommandHandler(IRepository<Category> categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public async Task Handle(UpdateCategoryCommand command)
+        {
+            var value=await _categoryRepository.GetById(command.CategoryId);
+            value.Name = command.Name;
+            await _categoryRepository.Update(value);
+        }
+    }
+}
