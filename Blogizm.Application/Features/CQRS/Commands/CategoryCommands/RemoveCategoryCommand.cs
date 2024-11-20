b8aa7586960fe96b6b255f33f66a3b4d13886e08
boratln <boratulan123@gmail.com>
2024-11-20 23:17:17 +0300

    CQRS RemoveCategoryCommand

---
+++ Blogizm.Application/Features/CQRS/Commands/CategoryCommands/RemoveCategoryCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Commands.CategoryCommands
+{
+    public class RemoveCategoryCommand
+    {
+       public int Id {  get; set; }
+
+        public RemoveCategoryCommand(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
