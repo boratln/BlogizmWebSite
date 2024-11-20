00bc347dbb1bb07264b87ea961e78df06197ad0a
boratln <boratulan123@gmail.com>
2024-11-20 23:19:49 +0300

    Mediator RemoveBlogCategoryCommand

---
+++ Blogizm.Application/Features/Mediator/Commands/BlogCategoryCommands/RemoveBlogCategoryCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Commands.BlogCategoryCommands
+{
+    public class RemoveBlogCategoryCommand:IRequest
+    {
+        public int Id { get; set; }
+
+        public RemoveBlogCategoryCommand(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
