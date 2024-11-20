834b3fb947e1ad3916e3a381abdeab94de858938
boratln <boratulan123@gmail.com>
2024-11-20 23:17:50 +0300

    CQRS GetCategoryByIdQueryHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/CategoryHandlers/GetCategoryByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Blogizm.Application.Features.CQRS.Queries.CategoryQueries;
+using Blogizm.Application.Features.CQRS.Results.CategoryResults;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.CategoryHandlers
+{
+    public class GetCategoryByIdQueryHandler
+    {
+        private readonly IRepository<Category> _categoryRepository;
+
+        public GetCategoryByIdQueryHandler(IRepository<Category> categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public async Task<GetCategoryByIdQueryResult> Handle(GetCategoryByIdQuery query)
+        {
+            var value = await _categoryRepository.GetById(query.Id);
+            return new GetCategoryByIdQueryResult
+            {
+                CategoryId = value.CategoryId,
+                Name = value.Name
+            };
+        }
+    }
+}
