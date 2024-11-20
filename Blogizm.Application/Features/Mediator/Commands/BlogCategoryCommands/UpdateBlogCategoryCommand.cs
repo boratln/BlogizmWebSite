8a66f45c4e4881a4b7dfec5b0b005f3c932aa362
boratln <boratulan123@gmail.com>
2024-11-20 23:20:09 +0300

    Mediator UpdateBlogCategoryCommand

---
+++ Blogizm.Application/Features/Mediator/Commands/BlogCategoryCommands/UpdateBlogCategoryCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Commands.BlogCategoryCommands
+{
+    public class UpdateBlogCategoryCommand:IRequest
+    {
+        public int BlogCategoryId { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
