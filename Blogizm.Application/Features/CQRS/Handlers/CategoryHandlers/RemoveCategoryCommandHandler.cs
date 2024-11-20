80f6d96c23d4bf23590464709e0ed1f01dcb07c4
boratln <boratulan123@gmail.com>
2024-11-20 23:18:25 +0300

    CQRS RemoveCategoryCommandHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/CategoryHandlers/RemoveCategoryCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Blogizm.Application.Features.CQRS.Commands.CategoryCommands;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.CategoryHandlers
+{
+    public class RemoveCategoryCommandHandler
+    {
+        private readonly IRepository<Category> _categoryRepository;
+
+        public RemoveCategoryCommandHandler(IRepository<Category> categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public async Task Handle(RemoveCategoryCommand command)
+        {
+            var value = await _categoryRepository.GetById(command.Id);
+            await _categoryRepository.Remove(value);
+        }
+    }
+}
