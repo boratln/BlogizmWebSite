28e556c38f3f6cdc995c6eedb497ce2e2bbabb14
boratln <boratulan123@gmail.com>
2024-11-20 23:17:00 +0300

    BlogCategoryRepository

---
+++ Blogizm.Persistence/Repositories/BlogCategoryRepositories/BlogCategoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using Blogizm.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Persistence.Repositories.BlogCategoryRepositories
+{
+    public class BlogCategoryRepository : IBlogCategoryRepository
+    {
+        private readonly BlogContext _blogContext;
+
+        public BlogCategoryRepository(BlogContext blogContext)
+        {
+            _blogContext = blogContext;
+        }
+
+        public async Task<List<BlogCategory>> GetAllBlogWithCategories()
+        {
+            var values = await _blogContext.BlogCategories.Include(x => x.Category).ToListAsync();
+            return values;
+        }
+    }
+}
