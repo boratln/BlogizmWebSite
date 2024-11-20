632d9fd0c6f7208997d174d50b73571e781d62ca
boratln <boratulan123@gmail.com>
2024-11-20 23:18:02 +0300

    CQRS GetCategoryQueryHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/CategoryHandlers/GetCategoryQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using Blogizm.Application.Features.CQRS.Results.CategoryResults;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.CategoryHandlers
+{
+    public class GetCategoryQueryHandler
+    {
+        private readonly IRepository<Category> _categoryRepository;
+
+        public GetCategoryQueryHandler(IRepository<Category> categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public async Task<List<GetCategoryQueryResult>> Handle()
+        {
+            var values=await _categoryRepository.GetAll();
+            return values.Select(x => new GetCategoryQueryResult
+            {
+                CategoryId = x.CategoryId,
+                Name = x.Name,
+            }).ToList();
+        }
+    }
+}
