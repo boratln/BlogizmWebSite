7909e36dd466bb0a3aa20a4389bb141d1b8f02ae
boratln <boratulan123@gmail.com>
2024-11-20 23:15:06 +0300

    Update Blog.cs

--- Blogizm.Domain/Entities/Blog.cs
+++ Blogizm.Domain/Entities/Blog.cs
@@ -9,15 +9,17 @@ namespace Blogizm.Domain.Entities
     public class Blog
     {
         public int BlogId { get; set; }
-        public int CategoryId {  get; set; }
-        public int TagId {  get; set; }
-        public Tag Tag { get; set; }
+        public int BlogCategoryId {  get; set; }
+      public BlogCategory BlogCategory { get; set; }
         public int AuthorId { get; set; }
-        public Author Author { get; set; }
-        public Category Category { get; set; }
+        public Author Author { get; set; }     
         public string Title {  get; set; }
         public string Description { get; set; }
-        
+        public string CoverImageUrl {  get; set; }
+        public DateTime CreatedDate {  get; set; }
+        public string BlogImage1 {  get; set; }
+        public string BlogImage2 {  get; set; }
+        public string BlogImage3 {  get; set; }
        
 
     }
