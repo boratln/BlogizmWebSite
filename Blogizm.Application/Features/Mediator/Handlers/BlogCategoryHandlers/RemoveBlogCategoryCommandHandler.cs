82068dc8522a30f993493df7eb346f81ebb46799
boratln <boratulan123@gmail.com>
2024-11-20 23:22:52 +0300

    Mediator RemoveBlogCategoryCommandHandler

---
+++ Blogizm.Application/Features/Mediator/Handlers/BlogCategoryHandlers/RemoveBlogCategoryCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Blogizm.Application.Features.Mediator.Commands.BlogCategoryCommands;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Handlers.BlogCategoryHandlers
+{
+    public class RemoveBlogCategoryCommandHandler : IRequestHandler<RemoveBlogCategoryCommand>
+    {
+        private readonly IRepository<BlogCategory> _blogCategoryrepository;
+
+        public RemoveBlogCategoryCommandHandler(IRepository<BlogCategory> blogCategoryrepository)
+        {
+            _blogCategoryrepository = blogCategoryrepository;
+        }
+
+        public async Task Handle(RemoveBlogCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _blogCategoryrepository.GetById(request.Id);
+            await _blogCategoryrepository.Remove(value);
+        }
+    }
+}
