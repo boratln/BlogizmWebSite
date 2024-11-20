608a4fc5447d8c116f740564c8233a76799878e2
boratln <boratulan123@gmail.com>
2024-11-20 23:17:37 +0300

    CQRS CreateCategoryCommandHandler

---
+++ Blogizm.Application/Features/CQRS/Handlers/CategoryHandlers/CreateCategoryCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Blogizm.Application.Features.CQRS.Commands.CategoryCommands;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.CQRS.Handlers.CategoryHandlers
+{
+    public class CreateCategoryCommandHandler
+    {
+        private readonly IRepository<Category> _categoryRepository;
+
+        public CreateCategoryCommandHandler(IRepository<Category> categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public async Task Handle(CreateCategoryCommand command)
+        {
+            await _categoryRepository.Create(new Category
+            {
+                Name = command.Name,
+            });
+        }
+    }
+}
