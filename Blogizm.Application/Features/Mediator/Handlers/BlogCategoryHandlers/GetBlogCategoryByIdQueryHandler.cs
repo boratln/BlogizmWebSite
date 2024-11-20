cfa46202f2b7a92187e657d37f9e52cd06bd87d5
boratln <boratulan123@gmail.com>
2024-11-20 23:23:19 +0300

    Mediator GetBlogCategoryByIdQueryHandler

---
+++ Blogizm.Application/Features/Mediator/Handlers/BlogCategoryHandlers/GetBlogCategoryByIdQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using Blogizm.Application.Features.Mediator.Queries.BlogCategoryQueries;
+using Blogizm.Application.Features.Mediator.Results.BlogCategoryResults;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Handlers.BlogCategoryHandlers
+{
+    public class GetBlogCategoryByIdQueryHandler : IRequestHandler<GetBlogCategoryByIdQuery, GetBlogCategoryByIdQueryResult>
+    {
+        private readonly IRepository<BlogCategory> _blogCategoryRepository;
+
+        public GetBlogCategoryByIdQueryHandler(IRepository<BlogCategory> blogCategoryRepository)
+        {
+            _blogCategoryRepository = blogCategoryRepository;
+        }
+
+        public async Task<GetBlogCategoryByIdQueryResult> Handle(GetBlogCategoryByIdQuery request, CancellationToken cancellationToken)
+        {
+            var value = await _blogCategoryRepository.GetById(request.Id);
+            return new GetBlogCategoryByIdQueryResult
+            {
+                BlogCategoryId = value.BlogCategoryId,
+                CategoryId = value.CategoryId,
+                Name = value.Name,
+            };
+        }
+    }
+}
