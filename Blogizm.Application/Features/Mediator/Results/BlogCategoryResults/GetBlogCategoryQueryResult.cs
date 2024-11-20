18b4a3d81c729045ab3258ab0f70a319d66c042e
boratln <boratulan123@gmail.com>
2024-11-20 23:20:56 +0300

    Mediator GetBlogCategoryQueryResult

---
+++ Blogizm.Application/Features/Mediator/Results/BlogCategoryResults/GetBlogCategoryQueryResult.cs
@@ -0,0 +1,16 @@
+﻿using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Results.BlogCategoryResults
+{
+    public class GetBlogCategoryQueryResult
+    {
+        public int BlogCategoryId { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
