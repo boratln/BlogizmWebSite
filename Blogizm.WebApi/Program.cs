117274c2360213df8aae757c13f2a913ca116f89
boratln <boratulan123@gmail.com>
2024-11-21 22:02:51 +0300

    Blog Repository Dependency Injection

--- Blogizm.WebApi/Program.cs
+++ Blogizm.WebApi/Program.cs
@@ -4,6 +4,9 @@ using Blogizm.Application.Services;
 using Blogizm.Persistence.Context;
 using Blogizm.Application.Containers;
 using Blogizm.Persistence.Repositories.BlogCategoryRepositories;
+using Blogizm.Application.Interfaces.BlogCategoryInterfaces;
+using Blogizm.Persistence.Repositories.BlogRepositories;
+using Blogizm.Application.Interfaces.BlogInterfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
@@ -17,6 +20,7 @@ builder.Services.AddApplicationService(builder.Configuration);
 //Repositories
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped(typeof(IBlogCategoryRepository), typeof(BlogCategoryRepository));
+builder.Services.AddScoped(typeof(IBlogRepository), typeof(BlogRepository));
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
