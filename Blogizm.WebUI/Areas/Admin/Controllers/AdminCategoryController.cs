2db473b8df9f0df1db8c316490addbe8f690234f
boratln <boratulan123@gmail.com>
2024-11-21 22:03:14 +0300

    Web UI Admin Category Controller

---
+++ Blogizm.WebUI/Areas/Admin/Controllers/AdminCategoryController.cs
@@ -0,0 +1,33 @@
+﻿using Blogizm.Dtos.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Blogizm.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminCategory")]
+    public class AdminCategoryController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCategoryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7023/api/Category/GetAllCategory");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
