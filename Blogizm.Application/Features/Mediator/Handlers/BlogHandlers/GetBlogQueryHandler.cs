685b726947bf69282bb8164e827f31926186252f
boratln <boratulan123@gmail.com>
2024-11-21 21:58:28 +0300

    Mediator GetBlogQueryHandler

---
+++ Blogizm.Application/Features/Mediator/Handlers/BlogHandlers/GetBlogQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using Blogizm.Application.Features.Mediator.Queries.BlogQueries;
+using Blogizm.Application.Features.Mediator.Results.BlogResults;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Handlers.BlogHandlers
+{
+    public class GetBlogQueryHandler : IRequestHandler<GetBlogQuery, List<GetBlogQueryResult>>
+    {
+        private readonly IRepository<Blog> _blogRepository;
+
+        public GetBlogQueryHandler(IRepository<Blog> blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<List<GetBlogQueryResult>> Handle(GetBlogQuery request, CancellationToken cancellationToken)
+        {
+           var values=await _blogRepository.GetAll();
+            return values.Select(x => new GetBlogQueryResult
+            {
+                AuthorId = x.AuthorId,
+                BlogCategoryId = x.BlogCategoryId,
+                BlogId = x.BlogId,
+                BlogImage1 = x.BlogImage1,
+                BlogImage2 = x.BlogImage2,
+                BlogImage3 = x.BlogImage3,
+                CoverImageUrl = x.CoverImageUrl,
+                CreatedDate = x.CreatedDate,
+                Description = x.Description,
+                Title = x.Title
+            }).ToList();
+        }
+    }
+}
