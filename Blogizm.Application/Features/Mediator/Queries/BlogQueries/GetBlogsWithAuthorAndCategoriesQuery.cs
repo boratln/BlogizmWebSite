75993a1d03324ee5b7d793f90b08bd8a0d84bb55
boratln <boratulan123@gmail.com>
2024-11-21 22:00:07 +0300

    Mediator GetBlogsWithAuthorAndCategoriesQuery

---
+++ Blogizm.Application/Features/Mediator/Queries/BlogQueries/GetBlogsWithAuthorAndCategoriesQuery.cs
@@ -0,0 +1,14 @@
+﻿using Blogizm.Application.Features.Mediator.Results.BlogResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Queries.BlogQueries
+{
+    public class GetBlogsWithAuthorAndCategoriesQuery:IRequest<List<GetBlogsWithAuthorAndCategoriesQueryResult>>
+    {
+    }
+}
