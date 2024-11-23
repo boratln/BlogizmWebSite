910a8012774192f49ce6de9045d6149eb3ef00f5
boratln <boratulan123@gmail.com>
2024-11-23 22:46:42 +0300

    Web UI AdminAboutController

---
+++ Blogizm.WebUI/Areas/Admin/Controllers/AdminAboutController.cs
@@ -0,0 +1,108 @@
+﻿using Blogizm.Dtos.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Blogizm.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminAbout")]
+    public class AdminAboutController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminAboutController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7023/api/About/GetAllAbout");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [Route("CreateAbout")]
+        [HttpGet]
+        public IActionResult CreateAbout()
+        {
+            return View();
+        }
+        [Route("CreateAbout")]
+        [HttpPost]
+        public async Task<IActionResult> CreateAbout(CreateAboutDto AboutDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var json = JsonConvert.SerializeObject(AboutDto);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7023/api/About/CreateAbout", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                TempData["baslik"] = "Hakkımızda kısmı Eklendi";
+                TempData["mesaj"] = "Hakkımızda kısmı başarılı bir şekilde eklendi";
+                return RedirectToAction("Index", "AdminAbout", new { area = "Admin" });
+            }
+            return RedirectToAction("Index", "AdminAbout", new { area = "Admin" });
+
+        }
+        [Route("RemoveAbout/{id}")]
+        public async Task<IActionResult> RemoveAbout(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMessage = await client.DeleteAsync("https://localhost:7023/api/About/DeleteAbout/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                TempData["baslik"] = "Hakkımızda kısmı Silindi";
+                TempData["mesaj"] = "Hakkımızda kısmı başarılı bir şekilde silindi";
+                return RedirectToAction("Index", "AdminAbout", new { area = "Admin" });
+            }
+            return RedirectToAction("Index", "AdminAbout", new { area = "Admin" });
+        }
+        [Route("UpdateAbout/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateAbout(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMessage = await client.GetAsync("https://localhost:7023/api/About/GetAbout/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<UpdateAboutDto>(jsonData);
+
+
+                return View(value);
+            }
+            return View(new UpdateAboutDto());
+
+        }
+        [Route("UpdateAbout/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateAbout(UpdateAboutDto AboutDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var json = JsonConvert.SerializeObject(AboutDto);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7023/api/About/UpdateAbout", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                TempData["baslik"] = "Hakkımızda kısmı Güncellendi";
+                TempData["mesaj"] = "Hakkımızda kısmı başarılı bir şekilde güncellendi";
+                return RedirectToAction("Index", "AdminAbout", new { area = "Admin" });
+            }
+            return RedirectToAction("Index", "AdminAbout", new { area = "Admin" });
+        }
+    }
+}
