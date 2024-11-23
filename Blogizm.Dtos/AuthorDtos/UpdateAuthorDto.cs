5189c421cebfd855f5ee3dc6ae730c673afa41cb
boratln <boratulan123@gmail.com>
2024-11-23 22:46:16 +0300

    Web UI UpdateAuthorDto

---
+++ Blogizm.Dtos/AuthorDtos/UpdateAuthorDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Dtos.AuthorDtos
+{
+    public class UpdateAuthorDto
+    {
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Description { get; set; }
+        public string AuthorImage { get; set; }
+    }
+}
