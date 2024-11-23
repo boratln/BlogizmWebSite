aa1ec5a5d9e5ebb788b53651be2a948525b53514
boratln <boratulan123@gmail.com>
2024-11-23 22:45:52 +0300

    Web UI CreateAuthorDto

---
+++ Blogizm.Dtos/AuthorDtos/CreateAuthorDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Dtos.AuthorDtos
+{
+    public class CreateAuthorDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Description { get; set; }
+        public string AuthorImage { get; set; }
+    }
+}
