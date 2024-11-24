4d5e2b32821fe1eb58d1edcfb66123b9253cd24c
boratln <boratulan123@gmail.com>
2024-11-24 23:27:51 +0300

    Web UI UpdateClientDto

---
+++ Blogizm.Dtos/ClientDtos/UpdateClientDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Dtos.ClientDtos
+{
+    public class UpdateClientDto
+    {
+        public int ClientId { get; set; }
+        public string ClientName { get; set; }
+        public string ClientDesc { get; set; }
+        public string ClientImage { get; set; }
+    }
+}
