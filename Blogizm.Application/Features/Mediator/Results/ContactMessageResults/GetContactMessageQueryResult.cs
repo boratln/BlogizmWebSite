046e109a8a04117242d95b54765ca6c00153390e
boratln <boratulan123@gmail.com>
2024-11-25 22:40:14 +0300

    Mediator GetContactMessageQueryResult

---
+++ Blogizm.Application/Features/Mediator/Results/ContactMessageResults/GetContactMessageQueryResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Results.ContactMessageResults
+{
+    public class GetContactMessageQueryResult
+    {
+        public int ContactMessageId { get; set; }
+        public string NameSurname { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public bool IsReaded { get; set; }
+    }
+}
