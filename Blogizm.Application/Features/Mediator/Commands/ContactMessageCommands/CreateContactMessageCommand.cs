cc261fd06d1e76bc1bb35e57118179e02a69a0b2
boratln <boratulan123@gmail.com>
2024-11-25 22:34:56 +0300

    Mediator CreateContactMessageCommand

---
+++ Blogizm.Application/Features/Mediator/Commands/ContactMessageCommands/CreateContactMessageCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Commands.ContactMessageCommands
+{
+    public class CreateContactMessageCommand:IRequest
+    {
+        public string NameSurname { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public bool IsReaded { get; set; }
+    }
+}
