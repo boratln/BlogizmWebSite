7c04969f3c5f6eeeb5e91f56e8325b493fca5dad
boratln <boratulan123@gmail.com>
2024-11-25 22:46:17 +0300

    Web API ContactMessageController

---
+++ Blogizm.WebApi/Controllers/ContactMessageController.cs
@@ -0,0 +1,37 @@
+﻿using Blogizm.Application.Features.Mediator.Commands.ContactMessageCommands;
+using Blogizm.Application.Features.Mediator.Queries.ContactMessageQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogizm.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactMessageController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ContactMessageController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet("GetContactMessage")]
+        public async Task<IActionResult> GetContactMessage()
+        {
+            var values = await _mediator.Send(new GetContactMessageQuery());
+            return Ok(values);
+        }
+        [HttpPost("CreateContactMessage")]
+        public async Task<IActionResult> CreateContactMessage(CreateContactMessageCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Mesaj başarıyla eklendi");
+        }
+        [HttpDelete("RemoveContactMessage/{id}")]
+        public async Task<IActionResult> RemoveContactMessage(int id) { 
+        await _mediator.Send(new RemoveContactMessageCommand(id));
+            return Ok("Mesaj silindi");
+        }
+    }
+}
