f6a33a73e3ea0da98507947ffb97d0512dc7d73c
boratln <boratulan123@gmail.com>
2024-11-26 19:13:04 +0300

    GetAllBlogCategoryWithGroupByDto

---
+++ Blogizm.Application/Dtos/BlogCategoryDtos/GetAllBlogCategoryWithGroupByDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Dtos.BlogCategoryDtos
+{
+    public class GetAllBlogCategoryWithGroupByDto
+    {
+        public int BlogCategoryId {  get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string BlogCategoryName { get; set; }
+    }
+}
