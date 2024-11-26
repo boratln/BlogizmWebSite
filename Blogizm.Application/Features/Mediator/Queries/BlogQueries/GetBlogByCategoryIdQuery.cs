a1d75c0476fcd47e3dbffd89739c7ad0280cc497
boratln <boratulan123@gmail.com>
2024-11-26 19:15:18 +0300

    Mediator GetBlogByCategoryIdQuery

---
+++ Blogizm.Application/Features/Mediator/Queries/BlogQueries/GetBlogByCategoryIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using Blogizm.Application.Features.Mediator.Results.BlogResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Queries.BlogQueries
+{
+    public class GetBlogByCategoryIdQuery:IRequest<List<GetBlogByCategoryIdQueryResult>>
+    {
+        public int Id { get; set; }
+
+        public GetBlogByCategoryIdQuery(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
