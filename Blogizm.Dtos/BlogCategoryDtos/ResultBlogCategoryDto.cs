ea7c0fee6a18402150eaf6043bf99c0a1b46553f
boratln <boratulan123@gmail.com>
2024-11-26 19:18:58 +0300

    WebUI ResultBlogCategoryDto

---
+++ Blogizm.Dtos/BlogCategoryDtos/ResultBlogCategoryDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Dtos.BlogCategoryDtos
+{
+    public class ResultBlogCategoryDto
+    {
+        public int BlogCategoryId {  get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string BlogCategoryName { get; set; }
+    }
+}
