0b28263fc528f482b5fce49e9639a2c7d8d0b37b
boratln <boratulan123@gmail.com>
2024-11-26 19:13:52 +0300

    Mediator GetBlogCategoryWithCategoryIdQueryHandler

---
+++ Blogizm.Application/Features/Mediator/Handlers/BlogCategoryHandlers/GetBlogCategoryWithCategoryIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using Blogizm.Application.Features.Mediator.Queries.BlogCategoryQueries;
+using Blogizm.Application.Features.Mediator.Results.BlogCategoryResults;
+using Blogizm.Application.Interfaces.BlogCategoryInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Handlers.BlogCategoryHandlers
+{
+    public class GetBlogCategoryWithCategoryIdQueryHandler : IRequestHandler<GetBlogCategoryWithCategoryIdQuery, List<GetBlogCategoryWithCategoryIdQueryResult>>
+    {
+        private readonly IBlogCategoryRepository _blogCategoryRepository;
+
+        public GetBlogCategoryWithCategoryIdQueryHandler(IBlogCategoryRepository blogCategoryRepository)
+        {
+            _blogCategoryRepository = blogCategoryRepository;
+        }
+
+        public async Task<List<GetBlogCategoryWithCategoryIdQueryResult>> Handle(GetBlogCategoryWithCategoryIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _blogCategoryRepository.GetBlogCategoryWithCategoryId(request.Id);
+            return values.Select(x => new GetBlogCategoryWithCategoryIdQueryResult
+            {
+                BlogCategoryId = x.BlogCategoryId,
+                CategoryId = x.CategoryId,
+                Name = x.Name
+            }).ToList();
+        }
+    }
+}
