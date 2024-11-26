62b94ecb8e1da0e8d3c8224206cc6a9d9710a749
boratln <boratulan123@gmail.com>
2024-11-26 19:19:07 +0300

    Web UI CreateBlogCategoryDto

---
+++ Blogizm.Dtos/BlogCategoryDtos/CreateBlogCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Dtos.BlogCategoryDtos
+{
+    public class CreateBlogCategoryDto
+    {
+        public int CategoryId { get; set; }     
+        public string Name { get; set; }
+    }
+}
