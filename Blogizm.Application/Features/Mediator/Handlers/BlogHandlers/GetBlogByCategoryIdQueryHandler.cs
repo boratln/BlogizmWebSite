30eb431b2e7535477d1fee827e9605b23947cd1f
boratln <boratulan123@gmail.com>
2024-11-26 19:14:06 +0300

    Mediator GetBlogByCategoryIdQueryHandler

---
+++ Blogizm.Application/Features/Mediator/Handlers/BlogHandlers/GetBlogByCategoryIdQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using Blogizm.Application.Features.Mediator.Queries.BlogQueries;
+using Blogizm.Application.Features.Mediator.Results.BlogResults;
+using Blogizm.Application.Interfaces.BlogInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Handlers.BlogHandlers
+{
+    public class GetBlogByCategoryIdQueryHandler : IRequestHandler<GetBlogByCategoryIdQuery, List<GetBlogByCategoryIdQueryResult>>
+    {
+        private readonly IBlogRepository _blogRepository;
+
+        public GetBlogByCategoryIdQueryHandler(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<List<GetBlogByCategoryIdQueryResult>> Handle(GetBlogByCategoryIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _blogRepository.GetBlogByCategoryId(request.Id);
+            return values.Select(x => new GetBlogByCategoryIdQueryResult
+            {
+                AuthorId = x.AuthorId,
+                BlogCategoryId = x.BlogCategoryId,
+                BlogId = x.BlogId,
+                BlogImage1 = x.BlogImage1,
+                BlogImage2 = x.BlogImage2,
+                BlogImage3 = x.BlogImage3,
+                CategoryId = x.BlogCategory.CategoryId,
+                CoverImageUrl = x.CoverImageUrl,
+                CreatedDate = x.CreatedDate,
+                Description = x.Description,
+                Title = x.Title
+            }).ToList();
+
+        }
+    }
+}
