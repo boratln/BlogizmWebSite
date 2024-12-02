1a7a45e13b9c995b7c14878e53c28c4bc4b3f5e0
boratln <boratulan123@gmail.com>
2024-12-02 22:48:59 +0300

    Web UI ContactMessageComponentPartial.cs

---
+++ Blogizm.WebUI/ViewComponents/ContactComponents/_ContactMessageComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blogizm.WebUI.ViewComponents.ContactComponents
+{
+    public class _ContactMessageComponentPartial:ViewComponent
+    {
+   
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
