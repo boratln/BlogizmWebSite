da6d3bc74f5fdc1dc1189ec0d09e6a80373ff48a
boratln <boratulan123@gmail.com>
2024-12-02 22:48:32 +0300

    Web UI AboutBannerComponentPartial.cs

---
+++ Blogizm.WebUI/ViewComponents/AboutComponents/_AboutBannerComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using Blogizm.Dtos.AboutBannerDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Blogizm.WebUI.ViewComponents.AboutComponents
+{
+    public class _AboutBannerComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AboutBannerComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7023/api/AboutBanner/GetAboutBanner/2");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<ResultAboutBannerDto>(jsonData);
+                return View(value);
+            }
+            return View(new ResultAboutBannerDto());
+        }
+    }
+}
