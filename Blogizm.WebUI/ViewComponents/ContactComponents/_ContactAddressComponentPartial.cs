26ece7bc8e471f059ace51723c8de87fe404fce9
boratln <boratulan123@gmail.com>
2024-12-02 22:48:51 +0300

    Web UI ContactAddressComponentPartial.cs

---
+++ Blogizm.WebUI/ViewComponents/ContactComponents/_ContactAddressComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Blogizm.Dtos.ContactAddressDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Blogizm.WebUI.ViewComponents.ContactComponents
+{
+    public class _ContactAddressComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ContactAddressComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7023/api/ContactAddress/GetAllContactAddress");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultContactAddressDto>>(jsonData);
+                return View(values);
+            }
+            return View(new List<ResultContactAddressDto>());
+        }
+    }
+}
