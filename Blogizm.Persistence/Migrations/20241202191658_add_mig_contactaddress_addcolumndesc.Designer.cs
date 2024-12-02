66203b1a5431c37c020527ad927ae203fe651637
boratln <boratulan123@gmail.com>
2024-12-02 22:48:06 +0300

    Add Description Property

--- Blogizm.Persistence/Migrations/20241202191658_add_mig_contactaddress_addcolumndesc.Designer.cs
+++ Blogizm.Persistence/Migrations/20241202191658_add_mig_contactaddress_addcolumndesc.Designer.cs
@@ -4,6 +4,7 @@ using Blogizm.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Blogizm.Persistence.Migrations
 {
     [DbContext(typeof(BlogContext))]
-    partial class BlogContextModelSnapshot : ModelSnapshot
+    [Migration("20241202191658_add_mig_contactaddress_addcolumndesc")]
+    partial class add_mig_contactaddress_addcolumndesc
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Blogizm.Persistence/Migrations/20241202191658_add_mig_contactaddress_addcolumndesc.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blogizm.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_mig_contactaddress_addcolumndesc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "ContactAddresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "ContactAddresses");
+        }
+    }
+}
