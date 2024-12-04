e470b97ab73b8dc69cdf5ebb2cdc987f281704cd
boratln <boratulan123@gmail.com>
2024-12-04 22:07:39 +0300

    Update Program.cs

--- Blogizm.WebUI/Program.cs
+++ Blogizm.WebUI/Program.cs
@@ -1,8 +1,26 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+// Desteklenen kültürleri tanýmlayýn
+var supportedCultures = new[] { "en", "tr", "fr" }; // Örneðin Ýngilizce, Türkçe, Fransýzca
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture("tr"); 
+    options.SupportedCultures = supportedCultures.Select(culture => new CultureInfo(culture)).ToList();
+    options.SupportedUICultures = supportedCultures.Select(culture => new CultureInfo(culture)).ToList();
+
+    // Tarayýcý Dilini Kullan
+    options.RequestCultureProviders.Insert(0, new AcceptLanguageHeaderRequestCultureProvider());
+});
 builder.Services.AddCors(opt =>
 {
     opt.AddPolicy("CorsPolicy", builder =>
@@ -25,6 +43,13 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+var localizationOptions = app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value;
+app.Use(async (context, next) =>
+{
+    context.Items["SupportedCultures"] = localizationOptions.SupportedCultures;
+    await next.Invoke();
+});
+app.UseRequestLocalization(localizationOptions);
 
 app.UseRouting();
 
