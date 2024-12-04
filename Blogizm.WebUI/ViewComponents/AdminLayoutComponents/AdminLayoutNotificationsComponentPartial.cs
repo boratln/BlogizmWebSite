09bddbaf785265612ef7c90463a6632d75e9b1c1
boratln <boratulan123@gmail.com>
2024-12-04 22:08:18 +0300

    Create AdminLayoutNotificationsComponentPartial.cs

---
+++ Blogizm.WebUI/ViewComponents/AdminLayoutComponents/AdminLayoutNotificationsComponentPartial.cs
@@ -0,0 +1,38 @@
+﻿using Blogizm.Dtos.ContactMessageDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Blogizm.WebUI.ViewComponents.AdminLayoutComponents
+{
+    public class AdminLayoutNotificationsComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminLayoutNotificationsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task  <IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7023/api/ContactMessage/ContactMessageWhereIsReadableFalseCount");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<ContactMessageWhereIsReadableFalseCountDto>(jsonData);
+                var count = value.MessageCount;
+                ViewBag.count = count;
+            }
+            var client2 = _httpClientFactory.CreateClient();
+            var responseMessage2 = await client2.GetAsync("https://localhost:7023/api/ContactMessage/GetContactMessageWhereIsReadableFalse");
+            if (responseMessage2.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage2.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ContactMessageWhereIsReadableFalseDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
