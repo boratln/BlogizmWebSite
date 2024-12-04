245088e32e3d31afaa7917356b6d2268ad06219f
boratln <boratulan123@gmail.com>
2024-12-04 22:08:06 +0300

    Update AdminLayoutController.cs

--- Blogizm.WebUI/Controllers/AdminLayoutController.cs
+++ Blogizm.WebUI/Controllers/AdminLayoutController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blogizm.WebUI.Controllers
 {
     public class AdminLayoutController : Controller
     {
+        public IActionResult ChangeCulture(string culture, string returnUrl = "/")
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+
+            return LocalRedirect(returnUrl);
+        }
         public IActionResult Index()
         {
             return View();
