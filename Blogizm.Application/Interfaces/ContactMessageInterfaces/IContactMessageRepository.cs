65f080e816b76ffb78976caf9252cbdeb37ee2fa
boratln <boratulan123@gmail.com>
2024-12-04 22:06:40 +0300

    IContactMessageRepository Interface

---
+++ Blogizm.Application/Interfaces/ContactMessageInterfaces/IContactMessageRepository.cs
@@ -0,0 +1,15 @@
+﻿using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Interfaces.ContactMessageInterfaces
+{
+    public interface IContactMessageRepository
+    {
+        Task<List<ContactMessage>> GetContactMessageWhereIsReadableFalse();
+         int ContactMessageWhereIsReadableFalseCount();
+    }
+}
