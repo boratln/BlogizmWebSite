9e5d9644335954e60c98cc5367ff081f03022163
boratln <boratulan123@gmail.com>
2024-12-04 22:06:26 +0300

    Mediator GetContactMessageWhererIsReadableFalseQueryResult

---
+++ Blogizm.Application/Features/Mediator/Results/ContactMessageResults/GetContactMessageWhereIsReadableFalseQueryResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Results.ContactMessageResults
+{
+    public class GetContactMessageWhereIsReadableFalseQueryResult
+    {
+        public int ContactMessageId { get; set; }
+        public string NameSurname { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsReaded { get; set; }
+    }
+}
