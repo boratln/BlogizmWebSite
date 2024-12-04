0a682885093523875966a80dc8939219a2fccd0f
boratln <boratulan123@gmail.com>
2024-12-04 22:08:11 +0300

    Create BaseController.cs

---
+++ Blogizm.WebUI/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Globalization;
+
+namespace Blogizm.WebUI.Controllers
+{
+    public class BaseController : Controller
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            // HttpContext.Items içinden kültürleri al
+            var supportedCultures = HttpContext.Items["SupportedCultures"] as List<CultureInfo>;
+            ViewBag.SupportedCultures = supportedCultures;
+            base.OnActionExecuting(context);
+        }
+    }
+}
