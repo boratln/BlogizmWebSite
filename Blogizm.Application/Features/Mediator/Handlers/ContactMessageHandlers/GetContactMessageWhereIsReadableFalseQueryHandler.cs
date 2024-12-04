b4b789bd45c6c2c58a75708d70908e039a9c6fa9
boratln <boratulan123@gmail.com>
2024-12-04 22:06:15 +0300

    Mediator GetContactMessageWhererIsReadableFalseQueryHandler

---
+++ Blogizm.Application/Features/Mediator/Handlers/ContactMessageHandlers/GetContactMessageWhereIsReadableFalseQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using Blogizm.Application.Features.Mediator.Queries.ContactMessageQueries;
+using Blogizm.Application.Features.Mediator.Results.ContactMessageResults;
+using Blogizm.Application.Interfaces.ContactMessageInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Handlers.ContactMessageHandlers
+{
+    public class GetContactMessageWhereIsReadableFalseQueryHandler : IRequestHandler<GetContactMessageWhereIsReadableFalseQuery, List<GetContactMessageWhereIsReadableFalseQueryResult>>
+    {
+        private readonly IContactMessageRepository _contactMessageRepository;
+
+        public GetContactMessageWhereIsReadableFalseQueryHandler(IContactMessageRepository contactMessageRepository)
+        {
+            _contactMessageRepository = contactMessageRepository;
+        }
+
+        public async Task<List<GetContactMessageWhereIsReadableFalseQueryResult>> Handle(GetContactMessageWhereIsReadableFalseQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _contactMessageRepository.GetContactMessageWhereIsReadableFalse();
+            return values.Select(x => new GetContactMessageWhereIsReadableFalseQueryResult
+            {
+                ContactMessageId = x.ContactMessageId,
+                CreatedDate = x.CreatedDate,
+                Email = x.Email,
+                IsReaded = x.IsReaded,
+                Message = x.Message,
+                NameSurname = x.NameSurname,
+                Subject = x.Subject
+            }).ToList();
+
+        }
+    }
+}
