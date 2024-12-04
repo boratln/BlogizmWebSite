e40ebc3cb45ecb2b1f0a9136265d9126e7996bf6
boratln <boratulan123@gmail.com>
2024-12-04 22:05:29 +0300

    Mediator ContactMessageWhereIsReadableFalseCountQueryResult

---
+++ Blogizm.Application/Features/Mediator/Results/ContactMessageResults/ContactMessageWhereIsReadableFalseCountQueryResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Results.ContactMessageResults
+{
+    public class ContactMessageWhereIsReadableFalseCountQueryResult
+    {
+        public int MessageCount {  get; set; }
+    }
+}
