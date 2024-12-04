afc4ab44526ed1e6633ad857da2baa2b74e68c9d
boratln <boratulan123@gmail.com>
2024-12-04 22:07:48 +0300

    ContactMessageRepository

---
+++ Blogizm.Persistence/Repositories/ContactMessageRepositories/ContactMessageRepository.cs
@@ -0,0 +1,34 @@
+﻿using Blogizm.Application.Interfaces.ContactMessageInterfaces;
+using Blogizm.Domain.Entities;
+using Blogizm.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Persistence.Repositories.ContactMessageRepositories
+{
+    public class ContactMessageRepository : IContactMessageRepository
+    {
+        private readonly BlogContext _blogContext;
+
+        public ContactMessageRepository(BlogContext blogContext)
+        {
+            _blogContext = blogContext;
+        }
+
+        public int ContactMessageWhereIsReadableFalseCount()
+        {
+            var count = _blogContext.ContactMessages.Where(x => x.IsReaded == false).Count();
+            return count;
+        }
+
+        public async Task<List<ContactMessage>> GetContactMessageWhereIsReadableFalse()
+        {
+            var values = await _blogContext.ContactMessages.Where(x => x.IsReaded == false).ToListAsync();
+            return values;
+        }
+    }
+}
