129eccc97901707dfbad94cbcb29fa41b1d7fce5
boratln <boratulan123@gmail.com>
2024-12-04 22:05:42 +0300

    ContactMessageWhereIsReadableFalseCountDto

---
+++ Blogizm.Dtos/ContactMessageDtos/ContactMessageWhereIsReadableFalseCountDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Dtos.ContactMessageDtos
+{
+    public class ContactMessageWhereIsReadableFalseCountDto
+    {
+        public int MessageCount { get; set; }
+    }
+}
