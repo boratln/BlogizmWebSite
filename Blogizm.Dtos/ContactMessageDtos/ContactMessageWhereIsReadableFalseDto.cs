f5b7368a7a51712c8bbb0bbdf11e91dc17d57873
boratln <boratulan123@gmail.com>
2024-12-04 22:07:15 +0300

    Web UI GetContactMessageWhererIsReadableFalseDto

---
+++ Blogizm.Dtos/ContactMessageDtos/ContactMessageWhereIsReadableFalseDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Dtos.ContactMessageDtos
+{
+    public class ContactMessageWhereIsReadableFalseDto
+    {
+        public int ContactMessageId { get; set; }
+        public string NameSurname { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsReaded { get; set; }
+    }
+}
