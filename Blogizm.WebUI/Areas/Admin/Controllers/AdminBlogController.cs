e06f244caa2f22c19bf61916a383b554acb2c91b
boratln <boratulan123@gmail.com>
2024-12-05 23:51:34 +0300

    Update AdminBlogController.cs

--- Blogizm.WebUI/Areas/Admin/Controllers/AdminBlogController.cs
+++ Blogizm.WebUI/Areas/Admin/Controllers/AdminBlogController.cs
@@ -38,8 +38,7 @@ namespace Blogizm.WebUI.Areas.Admin.Controllers
         [HttpGet]
         public async Task<IActionResult> CreateBlog()
         {
-            //category idsi seçilecek
-            //blog category oto gelecek
+
             var client=_httpClientFactory.CreateClient();
             var categoryResponse = await client.GetAsync("https://localhost:7023/api/Category/GetAllCategory");
             if (categoryResponse.IsSuccessStatusCode)
@@ -109,7 +108,7 @@ namespace Blogizm.WebUI.Areas.Admin.Controllers
         {
             var client = _httpClientFactory.CreateClient();
 
-            var responseMessage = await client.DeleteAsync("https://localhost:7023/api/Blog/DeleteBlog/" + id);
+            var responseMessage = await client.DeleteAsync("https://localhost:7023/api/Blog/RemoveBlog/" + id);
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
