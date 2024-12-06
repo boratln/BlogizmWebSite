8b334a47fe3862faf9988709a5bf5ecf473071e4
boratln <boratulan123@gmail.com>
2024-12-07 01:05:28 +0300

    Update _UILayoutNavbarComponentPartial.cs

--- Blogizm.WebUI/ViewComponents/UILayoutComponents/_UILayoutNavbarComponentPartial.cs
+++ Blogizm.WebUI/ViewComponents/UILayoutComponents/_UILayoutNavbarComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blogizm.Dtos.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Blogizm.WebUI.ViewComponents.UILayoutComponents
 {
     public class _UILayoutNavbarComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpclientfactory;
+
+        public _UILayoutNavbarComponentPartial(IHttpClientFactory httpclientfactory)
         {
+            _httpclientfactory = httpclientfactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpclientfactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7023/api/Category/GetAllCategory");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
