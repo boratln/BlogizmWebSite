939bb095f4476519a5c1fc9a0b5a686d7e6326c1
boratln <boratulan123@gmail.com>
2024-12-06 21:47:30 +0300

    Web UI DefaultTop3Games Component Partial Class

---
+++ Blogizm.WebUI/ViewComponents/DefaultComponents/_DefaultTop3GamesComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using Blogizm.Dtos.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Blogizm.WebUI.ViewComponents.DefaultComponents
+{
+    public class _DefaultTop3GamesComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpclientfactory;
+
+        public _DefaultTop3GamesComponentPartial(IHttpClientFactory httpclientfactory)
+        {
+            _httpclientfactory = httpclientfactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpclientfactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7023/api/Blog/GetTop3BlogByCategoryId/1");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTop3GamesBlogDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
