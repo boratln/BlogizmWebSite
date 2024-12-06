5254d9cc263d177ddc67e00d42c3009c5ed42fbe
boratln <boratulan123@gmail.com>
2024-12-07 01:04:35 +0300

    Web UI BlogController

---
+++ Blogizm.WebUI/Controllers/BlogController.cs
@@ -0,0 +1,32 @@
+﻿using Blogizm.Dtos.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Blogizm.WebUI.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BlogController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        
+        public async Task<IActionResult> Index(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7023/api/Blog/GetBlogByCategoryId/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+               
+                var values = JsonConvert.DeserializeObject<List<ResultBlogDto>>(jsonData);
+                
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
