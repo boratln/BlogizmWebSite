1fd0979a0a01fdab4cb887a2c17f7ce7d7d16d6d
boratln <boratulan123@gmail.com>
2024-12-06 21:46:05 +0300

    ResultTop3GamesBlogDto

---
+++ Blogizm.Dtos/BlogDtos/ResultTop3GamesBlogDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Dtos.BlogDtos
+{
+    public class ResultTop3GamesBlogDto
+    {
+        public int BlogId { get; set; }
+        public int BlogCategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string BlogCategoryName { get; set; }
+        public int AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string CoverImageUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? BlogImage1 { get; set; }
+        public string? BlogImage2 { get; set; }
+        public string? BlogImage3 { get; set; }
+        public string? VideoUrl { get; set; }
+    }
+}
