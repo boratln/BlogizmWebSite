4ed7cd47783082f7a8385aaa71a807a038afc95d
boratln <boratulan123@gmail.com>
2024-12-06 21:45:12 +0300

    Mediator GetTop3BlogByCategoryIdQuery

---
+++ Blogizm.Application/Features/Mediator/Queries/BlogQueries/GetTop3BlogByCategoryIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using Blogizm.Application.Features.Mediator.Results.BlogResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Queries.BlogQueries
+{
+    public class GetTop3BlogByCategoryIdQuery:IRequest<List<GetTop3BlogByCategoryIdQueryResult>>
+    {
+        public int CategoryId {  get; set; }
+
+        public GetTop3BlogByCategoryIdQuery(int categoryId)
+        {
+            CategoryId = categoryId;
+        }
+    }
+}
