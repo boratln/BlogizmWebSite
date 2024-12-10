6a0d05ff76e09d1ac0f103282aa37aafc5dfdc7b
boratln <boratulan123@gmail.com>
2024-12-10 19:53:33 +0300

    Web UI _BlogDetailAuthorComponentPartial.cs

---
+++ Blogizm.WebUI/ViewComponents/BlogDetailComponents/_BlogDetailAuthorComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace Blogizm.WebUI.ViewComponents.BlogDetailComponents
+{
+    public class _BlogDetailAuthorComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
