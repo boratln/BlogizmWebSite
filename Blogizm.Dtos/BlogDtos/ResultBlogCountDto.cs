3e1af7b8ad74b436b41ea09a77d587683c31b466
boratln <boratulan123@gmail.com>
2024-12-10 19:51:42 +0300

    Web UI ResultBlogCountDto

---
+++ Blogizm.Dtos/BlogDtos/ResultBlogCountDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Dtos.BlogDtos
+{
+    public class ResultBlogCountDto
+    {
+        public int BlogCount { get; set; }
+
+    }
+}
