85aa96208aa22f76b4a32123a0a75480429c744c
boratln <boratulan123@gmail.com>
2024-12-10 19:52:29 +0300

    BlogAndBlogCategoryCountByCategoryIdQuery

---
+++ Blogizm.Application/Features/Mediator/Queries/BlogQueries/BlogAndBlogCategoryCountByCategoryIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using Blogizm.Application.Features.Mediator.Results.BlogResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Queries.BlogQueries
+{
+    public class BlogAndBlogCategoryCountByCategoryIdQuery:IRequest<List<BlogAndBlogCategoryCountByCategoryIdQueryResult>>
+    {
+        public int CategoryId { get; set; }
+
+        public BlogAndBlogCategoryCountByCategoryIdQuery(int categoryId)
+        {
+            CategoryId = categoryId;
+        }
+    }
+}
