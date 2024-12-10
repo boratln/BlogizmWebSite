c5bb19aa433f0fa04c854feb82f3549a89fab8a7
boratln <boratulan123@gmail.com>
2024-12-10 19:50:21 +0300

    Web UI _BlogCategoriesComponentPartial.cs

---
+++ Blogizm.WebUI/ViewComponents/BlogComponents/_BlogCategoriesComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Blogizm.Dtos.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Blogizm.WebUI.ViewComponents.BlogComponents
+{
+    public class _BlogCategoriesComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogCategoriesComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7023/api/Blog/BlogAndBlogCategoryCountByCategoryId/"+id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<BlogAndBlogCategoryCountByCategoryIdDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
