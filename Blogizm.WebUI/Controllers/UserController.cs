d41f511018e1a15223bef6bf1dce9b411d41c428
boratln <boratulan123@gmail.com>
2024-12-10 19:50:00 +0300

    Create UserController.cs

---
+++ Blogizm.WebUI/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blogizm.WebUI.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
