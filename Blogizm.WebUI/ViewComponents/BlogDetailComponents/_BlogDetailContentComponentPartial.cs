d64ef0cb29345ae9916298dad4e112b2293abbda
boratln <boratulan123@gmail.com>
2024-12-10 19:54:31 +0300

    Web UI _BlogDetailContentComponentPartial.cs

---
+++ Blogizm.WebUI/ViewComponents/BlogDetailComponents/_BlogDetailContentComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Blogizm.Dtos.BlogDetailDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Blogizm.WebUI.ViewComponents.BlogDetailComponents
+{
+    public class _BlogDetailContentComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailContentComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task <IViewComponentResult> InvokeAsync(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7023/api/Blog/GetBlogById/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultBlogDetailContentDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
