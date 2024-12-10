786edbd4ccabc04e35045b551a6087c789d9d234
boratln <boratulan123@gmail.com>
2024-12-10 19:51:57 +0300

    Web UI ResultBlogCountDto

---
+++ Blogizm.Dtos/BlogDtos/BlogAndBlogCategoryCountByCategoryIdDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Dtos.BlogDtos
+{
+    public class BlogAndBlogCategoryCountByCategoryIdDto
+    {
+        public int BlogCount { get; set; }
+        public string BlogCategoryName { get; set; }
+    }
+}
