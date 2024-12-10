3ab80a7012d4e9b5a7364135e0450255b34fdb2b
boratln <boratulan123@gmail.com>
2024-12-10 19:52:15 +0300

    GetBlogByCategoryIdCountQueryResult

---
+++ Blogizm.Application/Features/Mediator/Results/BlogResults/GetBlogByCategoryIdCountQueryResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Results.BlogResults
+{
+    public class GetBlogByCategoryIdCountQueryResult
+    {
+        public int BlogCount {  get; set; }
+    }
+}
