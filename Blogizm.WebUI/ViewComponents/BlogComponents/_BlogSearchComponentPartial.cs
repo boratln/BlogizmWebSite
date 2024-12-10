39852b5bd7e6390a23ded4750e9a0ee721f31f5d
boratln <boratulan123@gmail.com>
2024-12-10 19:53:17 +0300

    Web UI _BlogSearchComponentPartial.cs

---
+++ Blogizm.WebUI/ViewComponents/BlogComponents/_BlogSearchComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blogizm.WebUI.ViewComponents.BlogComponents
+{
+    public class _BlogSearchComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
