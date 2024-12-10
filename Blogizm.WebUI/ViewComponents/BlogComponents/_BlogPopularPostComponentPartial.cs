5c30a2a9f4ed98ac949aac0df30b55ef41c7e072
boratln <boratulan123@gmail.com>
2024-12-10 19:50:32 +0300

    Web UI _BlogPopularPostComponentPartial.cs

---
+++ Blogizm.WebUI/ViewComponents/BlogComponents/_BlogPopularPostComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blogizm.WebUI.ViewComponents.BlogComponents
+{
+    public class _BlogPopularPostComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
