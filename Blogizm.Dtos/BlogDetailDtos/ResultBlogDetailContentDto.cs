5c08084dcf42358c28622278fecd8208b17fe741
boratln <boratulan123@gmail.com>
2024-12-10 19:52:05 +0300

    Web UI ResultBlogDetailContentDto

---
+++ Blogizm.Dtos/BlogDetailDtos/ResultBlogDetailContentDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Dtos.BlogDetailDtos
+{
+    public class ResultBlogDetailContentDto
+    {
+        
+            public int blogId { get; set; }
+            public int blogCategoryId { get; set; }
+            public string title { get; set; }
+            public string description { get; set; }
+            public string coverImageUrl { get; set; }
+            public DateTime createdDate { get; set; }
+            public string blogImage1 { get; set; }
+            public string blogImage2 { get; set; }
+            public string blogImage3 { get; set; }
+            public object videoUrl { get; set; }
+        
+
+    }
+}
