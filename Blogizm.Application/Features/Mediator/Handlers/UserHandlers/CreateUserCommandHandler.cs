2b9311c0b999fea1938ee8e9e9e367d33be5d96a
boratln <boratulan123@gmail.com>
2024-12-11 14:55:33 +0300

    Mediator CreateUserCommandHandler

---
+++ Blogizm.Application/Features/Mediator/Handlers/UserHandlers/CreateUserCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using Blogizm.Application.Enums;
+using Blogizm.Application.Features.Mediator.Commands.UserCommands;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Handlers.UserHandlers
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand>
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public CreateUserCommandHandler(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            await _userRepository.Create(new User
+            {
+                UserName=request.Username,
+                Email = request.Email,
+                Name = request.Name,
+                RoleId=(int)RolesType.Member,
+                Password = request.Password,
+                Surname = request.Surname,
+                isConfirmed=false
+            });
+        }
+    }
+}
