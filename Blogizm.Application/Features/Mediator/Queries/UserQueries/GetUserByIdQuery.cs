51eebb95ac3fafc3948ae11e085a36a08da3840b
boratln <boratulan123@gmail.com>
2024-12-11 14:58:31 +0300

    Mediator GetUserByIdQuery

---
+++ Blogizm.Application/Features/Mediator/Queries/UserQueries/GetUserByIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using Blogizm.Application.Features.Mediator.Results.UserResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Queries.UserQueries
+{
+    public class GetUserByIdQuery:IRequest<GetUserByIdQueryResult>
+    {
+        public int Id { get; set; }
+
+        public GetUserByIdQuery(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
