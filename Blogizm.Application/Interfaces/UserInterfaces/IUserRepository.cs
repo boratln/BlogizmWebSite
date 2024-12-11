35df3557070c4a2b46f75501ccb8d608d7f07c37
boratln <boratulan123@gmail.com>
2024-12-11 14:57:25 +0300

    IUserRepository

---
+++ Blogizm.Application/Interfaces/UserInterfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Blogizm.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Interfaces.UserInterfaces
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetByFilter(Expression<Func<User, bool>> filter);
+
+    }
+}
