a912176b114a08bc44a283624f8b0c2c3a17a0d0
boratln <boratulan123@gmail.com>
2024-12-11 14:57:44 +0300

    Mediator GetUserByIdQueryHandler

---
+++ Blogizm.Application/Features/Mediator/Handlers/UserHandlers/GetUserByIdQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using Blogizm.Application.Features.Mediator.Queries.UserQueries;
+using Blogizm.Application.Features.Mediator.Results.UserResults;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Handlers.UserHandlers
+{
+    internal class GetUserByIdQueryHandler:IRequestHandler<GetUserByIdQuery,GetUserByIdQueryResult>
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public GetUserByIdQueryHandler(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<GetUserByIdQueryResult> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            var value = await _userRepository.GetById(request.Id);
+            return new GetUserByIdQueryResult
+            {
+                Email = value.Email,
+                isConfirmed = value.isConfirmed,
+                Name = value.Name,
+                Password = value.Password,
+                Surname = value.Surname,
+                UserId = value.UserId,
+            };
+        }
+    }
+}
