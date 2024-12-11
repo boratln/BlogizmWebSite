ebaa1c55063772630a1a6070438491bc25141654
boratln <boratulan123@gmail.com>
2024-12-11 14:58:49 +0300

    Mediator UpdateUserCommandHandler

---
+++ Blogizm.Application/Features/Mediator/Handlers/UserHandlers/UpdateUserCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Blogizm.Application.Features.Mediator.Commands.UserCommands;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Handlers.UserHandlers
+{
+    public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand>
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public UpdateUserCommandHandler(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _userRepository.GetById(request.UserId);
+            value.Surname = request.Surname;
+            value.Email = request.Email;
+            value.Name= request.Name;
+            await _userRepository.Update(value);
+        }
+    }
+}
