4fad6657b1f435964c2fd11c4d6fdf5edad6d24b
boratln <boratulan123@gmail.com>
2024-12-11 14:53:40 +0300

    TokenResponseDto

---
+++ Blogizm.Application/Dtos/UserDtos/TokenResponseDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Dtos.UserDtos
+{
+    public class TokenResponseDto
+    {
+        public string Token { get; set; }
+        public DateTime Expire_Date { get; set; }
+
+        public TokenResponseDto( string token, DateTime expire_Date)
+        {
+            Expire_Date = expire_Date;
+            Token = token;
+        }
+    }
+}
