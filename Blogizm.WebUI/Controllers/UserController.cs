50aaf7b3c79dc3024fc14cfc4e2df235ddee86e1
boratln <boratulan123@gmail.com>
2024-12-11 14:55:00 +0300

    Update UserController.cs

--- Blogizm.WebUI/Controllers/UserController.cs
+++ Blogizm.WebUI/Controllers/UserController.cs
@@ -1,14 +1,89 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using Blogizm.Dtos.UserDtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text;
+using Blogizm.WebUI.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Newtonsoft.Json;
 namespace Blogizm.WebUI.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public UserController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Login()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Login(UserLoginDto userLoginDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var content = new StringContent(System.Text.Json.JsonSerializer.Serialize(userLoginDto), Encoding.UTF8, "application/json");
+            var loginresponse = await client.PostAsync("https://localhost:7023/api/User/LoginUser", content);
+            if (loginresponse.IsSuccessStatusCode)
+            {
+                var jsonData=await loginresponse.Content.ReadAsStringAsync();
+                var tokenModel = System.Text.Json.JsonSerializer.Deserialize<JwtResponseModel>(jsonData, new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                });
+                if(tokenModel != null)
+                {
+                    JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+                    var token = handler.ReadJwtToken(tokenModel.Token);
+                    var claims = token.Claims.ToList();
+                    if (tokenModel.Token != null)
+                    {
+                        claims.Add(new Claim("accessToken", tokenModel.Token));
+                        var claimsIdentity = new ClaimsIdentity(claims, JwtBearerDefaults.AuthenticationScheme);
+                        var authProps = new AuthenticationProperties
+                        {
+                            ExpiresUtc = tokenModel.Expire_Date,
+                            IsPersistent = true
+                        };
+                        await HttpContext.SignInAsync(JwtBearerDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProps);
+                        TempData["accessToken"] = tokenModel.Token;
+
+                        return RedirectToAction("Index", "Default");
+                    }
+                }
+               
+            }
+            return View();
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(JwtBearerDefaults.AuthenticationScheme);
+            TempData.Clear();
+            return RedirectToAction("Index", "Default");
+        }
         public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterDto userRegisterDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(userRegisterDto);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7023/api/User/CreateUser", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            return RedirectToAction("Index", "Default");
+        }
+        public async Task<IActionResult> Profile()
         {
             return View();
         }
