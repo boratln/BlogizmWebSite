a8a5f3dd95c85c8e91d123d10b83b2aaed4165c7
boratln <boratulan123@gmail.com>
2024-12-11 14:55:25 +0300

    Mediator UpdateUserCommand

---
+++ Blogizm.Application/Features/Mediator/Commands/UserCommands/UpdateUserCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Commands.UserCommands
+{
+    public class UpdateUserCommand:IRequest
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool isConfirmed { get; set; }
+    }
+}
