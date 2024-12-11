2a2b5d7a9d7bef284a903db8f9c258d95439b259
boratln <boratulan123@gmail.com>
2024-12-11 14:56:37 +0300

    Web UI CommentController

---
+++ Blogizm.WebUI/Controllers/CommentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blogizm.WebUI.Controllers
+{
+    public class CommentController : Controller
+    {
+        public IActionResult CreateComment()
+        {
+            return View();
+        }
+    }
+}
