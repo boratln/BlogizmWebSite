0125570c06e92b7a5eb807cdcae0a8528e30d6eb
boratln <boratulan123@gmail.com>
2024-12-11 14:56:25 +0300

    Jwt Token Defaults

---
+++ Blogizm.Application/Tools/JwtTokenDefaults.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Tools
+{
+    internal class JwtTokenDefaults
+    {
+        public const string ValidAudience = "https://192.168.1.93";
+        public const string ValidIssiuer = "https://192.168.1.93";
+        public const string Key = "Blogizm01Blogizm014548+BlogizmProje01214+";
+        public const int Expire = 3;
+    }
+}
