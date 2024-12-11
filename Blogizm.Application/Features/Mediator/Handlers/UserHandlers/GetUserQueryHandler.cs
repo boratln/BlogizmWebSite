4ec91bccfb28a165daac7aadc33e4e2ec20af0bd
boratln <boratulan123@gmail.com>
2024-12-11 14:57:52 +0300

    Mediator GetUserQueryHandler

---
+++ Blogizm.Application/Features/Mediator/Handlers/UserHandlers/GetUserQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using Blogizm.Application.Features.Mediator.Queries.UserQueries;
+using Blogizm.Application.Features.Mediator.Results.UserResults;
+using Blogizm.Application.Interfaces;
+using Blogizm.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Handlers.UserHandlers
+{
+    public class GetUserQueryHandler : IRequestHandler<GetUserQuery, List<GetUserQueryResult>>
+    {
+        private readonly IRepository<User>_userRepository;
+
+        public GetUserQueryHandler(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<List<GetUserQueryResult>> Handle(GetUserQuery request, CancellationToken cancellationToken)
+        {
+           var values=await _userRepository.GetAll();
+            return values.Select(x => new GetUserQueryResult
+            {
+                Email = x.Email,
+                isConfirmed = x.isConfirmed,
+                Name = x.Name,
+                Password = x.Password,
+                Surname = x.Surname,
+                UserId = x.UserId,
+            }).ToList();
+        }
+    }
+}
