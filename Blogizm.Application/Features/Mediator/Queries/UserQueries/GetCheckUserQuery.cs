5e040620acf72051312d0218ada83f90ac0db9a0
boratln <boratulan123@gmail.com>
2024-12-11 14:58:41 +0300

    Mediator GetCheckUserQuery

---
+++ Blogizm.Application/Features/Mediator/Queries/UserQueries/GetCheckUserQuery.cs
@@ -0,0 +1,16 @@
+﻿using Blogizm.Application.Features.Mediator.Results.UserResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Queries.UserQueries
+{
+    public class GetCheckUserQuery:IRequest<GetCheckUserQueryResult>
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
