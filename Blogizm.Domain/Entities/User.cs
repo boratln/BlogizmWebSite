d734519869b722f0dba328dc4aaa8c9c8f58ad6d
boratln <boratulan123@gmail.com>
2024-12-11 14:57:16 +0300

    Update User.cs

--- Blogizm.Domain/Entities/User.cs
+++ Blogizm.Domain/Entities/User.cs
@@ -9,10 +9,13 @@ namespace Blogizm.Domain.Entities
     public class User
     {
         public int UserId { get; set; }
+        public string UserName { get; set; }
         public string Name { get; set; }
         public string Surname {  get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public int RoleId {  get; set; }
+        public Role Role { get; set; }
         public bool isConfirmed { get; set; }
     }
 }
