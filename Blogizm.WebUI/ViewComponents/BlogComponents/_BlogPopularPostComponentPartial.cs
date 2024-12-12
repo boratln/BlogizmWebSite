e16d342082f75ca15590f2cd3d136d08ad4a1a27
boratln <boratulan123@gmail.com>
2024-12-12 20:19:23 +0300

    Update _BlogPopularPostComponentPartial.cs

--- Blogizm.WebUI/ViewComponents/BlogComponents/_BlogPopularPostComponentPartial.cs
+++ Blogizm.WebUI/ViewComponents/BlogComponents/_BlogPopularPostComponentPartial.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blogizm.Dtos.BlogDetailDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Blogizm.WebUI.ViewComponents.BlogComponents
 {
     public class _BlogPopularPostComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogPopularPostComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int categoryid)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7023/api/Blog/GetTop3BlogByCategoryId/" + categoryid);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<Get3BlogByCategoryIdDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
+
     }
 }
