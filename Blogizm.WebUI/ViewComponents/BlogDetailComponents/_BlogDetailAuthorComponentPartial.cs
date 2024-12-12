0f22564abeae43e6a18189d898fc6e9e275ef11a
boratln <boratulan123@gmail.com>
2024-12-12 20:19:17 +0300

    added API endpoint

--- Blogizm.WebUI/ViewComponents/BlogDetailComponents/_BlogDetailAuthorComponentPartial.cs
+++ Blogizm.WebUI/ViewComponents/BlogDetailComponents/_BlogDetailAuthorComponentPartial.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blogizm.Dtos.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.ComponentModel;
 
 namespace Blogizm.WebUI.ViewComponents.BlogDetailComponents
 {
     public class _BlogDetailAuthorComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailAuthorComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async  Task<IViewComponentResult> InvokeAsync(int blogid)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7023/api/Blog/GetAuthorByBlogId/"+blogid);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var author = JsonConvert.DeserializeObject<GetAuthorByBlogIdDto>(jsonData);
+                return View(author);
+            }
             return View();
         }
     }
