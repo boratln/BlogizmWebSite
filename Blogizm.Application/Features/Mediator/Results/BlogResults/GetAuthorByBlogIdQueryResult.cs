b551643eecff6673f02ddfb42e253df5c73baec5
boratln <boratulan123@gmail.com>
2024-12-12 20:19:41 +0300

    Mediator GetAuthorByBlogIdQueryResult

---
+++ Blogizm.Application/Features/Mediator/Results/BlogResults/GetAuthorByBlogIdQueryResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Results.BlogResults
+{
+    public class GetAuthorByBlogIdQueryResult
+    {
+        public int BlogId {  get; set; }
+        public int AuthorId { get; set; }
+        public string AuthorFullname {  get; set; }
+        public string AuthorDescription {  get; set; }
+        public string AuthorImage {  get; set; }
+    }
+}
