3ad764b275b1a03ea7f78d2a112b3e222b65c2a2
boratln <boratulan123@gmail.com>
2024-12-12 20:14:50 +0300

    Web UI GetAuthorByBlogIdDto

---
+++ Blogizm.Dtos/BlogDtos/GetAuthorByBlogIdDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Dtos.BlogDtos
+{
+    public class GetAuthorByBlogIdDto
+    {
+        public int BlogId { get; set; }
+        public int AuthorId { get; set; }
+        public string AuthorFullname { get; set; }
+        public string AuthorDescription { get; set; }
+        public string AuthorImage { get; set; }
+    }
+}
