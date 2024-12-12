08102dc71e1cea9eb89d82df161e1e20cac30a6b
boratln <boratulan123@gmail.com>
2024-12-12 20:13:58 +0300

    Mediator Get3BlogByCategoryIdinNotCurrentBlogIdQuery

---
+++ Blogizm.Application/Features/Mediator/Queries/BlogQueries/Get3BlogByCategoryIdinNotCurrentBlogIdQuery.cs
@@ -0,0 +1,22 @@
+﻿using Blogizm.Application.Features.Mediator.Results.BlogResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Queries.BlogQueries
+{
+    public class Get3BlogByCategoryIdinNotCurrentBlogIdQuery:IRequest<List<Get3BlogByCategoryIdinNotCurrentBlogIdQueryResult>>
+    {
+        public int BlogId { get; set; }
+        public int CategoryId { get; set; }
+
+        public Get3BlogByCategoryIdinNotCurrentBlogIdQuery(int categoryId, int blogId)
+        {
+            CategoryId = categoryId;
+            BlogId = blogId;
+        }
+    }
+}
