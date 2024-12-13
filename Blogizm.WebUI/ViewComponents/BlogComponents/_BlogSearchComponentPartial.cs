1bb46c81a76ab76bb7a92cdbc780e23eb6b67e2c
boratln <boratulan123@gmail.com>
2024-12-13 16:43:57 +0300

    Update _BlogSearchComponentPartial.cs

--- Blogizm.WebUI/ViewComponents/BlogComponents/_BlogSearchComponentPartial.cs
+++ Blogizm.WebUI/ViewComponents/BlogComponents/_BlogSearchComponentPartial.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blogizm.Dtos.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace Blogizm.WebUI.ViewComponents.BlogComponents
 {
     public class _BlogSearchComponentPartial:ViewComponent
     {
+    
         public IViewComponentResult Invoke()
         {
+      
             return View();
         }
     }
