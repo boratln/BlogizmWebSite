dd72222bad77e5cc428ef847bd7dba6a83bb69b7
boratln <boratulan123@gmail.com>
2024-12-13 16:44:36 +0300

    Web UI _UILayoutFooterSocialMediaComponentPartial.cs

---
+++ Blogizm.WebUI/ViewComponents/UILayoutComponents/_UILayoutFooterSocialMediaComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Blogizm.Dtos.SocialMediaDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Blogizm.WebUI.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutFooterSocialMediaComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _UILayoutFooterSocialMediaComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async  Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7023/api/SocialMedia/GetAllSocialMedia");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<List<ResultSocialMediaDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
