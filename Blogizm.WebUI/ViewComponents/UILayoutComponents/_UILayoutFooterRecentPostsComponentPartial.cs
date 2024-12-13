3773d3360ce3de4c3f97cbdc49eabf45f397bd57
boratln <boratulan123@gmail.com>
2024-12-13 16:47:15 +0300

    Web UI _UILayoutFooterRecentPostsComponentPartial.cs

---
+++ Blogizm.WebUI/ViewComponents/UILayoutComponents/_UILayoutFooterRecentPostsComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using Blogizm.Dtos.BlogDtos;
+using Blogizm.Dtos.SocialMediaDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace Blogizm.WebUI.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutFooterRecentPostsComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _UILayoutFooterRecentPostsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7023/api/Blog/GetLast3Blog");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLast3BlogDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
