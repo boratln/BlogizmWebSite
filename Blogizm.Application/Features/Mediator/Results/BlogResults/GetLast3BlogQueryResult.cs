3d5887e610b5babdbd9ee0c516ccfe0fa645f55e
boratln <boratulan123@gmail.com>
2024-12-13 16:47:23 +0300

    Mediator GetLast3BlogQueryResult

---
+++ Blogizm.Application/Features/Mediator/Results/BlogResults/GetLast3BlogQueryResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Results.BlogResults
+{
+    public class GetLast3BlogQueryResult
+    {
+        public int BlogId { get; set; }
+        public int BlogCategoryId { get; set; }
+        public int CategoryId {  get; set; }
+        public int AuthorId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string SubDescription { get; set; }
+        public string CoverImageUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? BlogImage1 { get; set; }
+        public string? BlogImage2 { get; set; }
+        public string? BlogImage3 { get; set; }
+        public string? VideoUrl { get; set; }
+    }
+}
