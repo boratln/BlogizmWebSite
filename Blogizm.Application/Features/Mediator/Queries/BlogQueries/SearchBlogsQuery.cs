99049858fb24dd3a9d862829499e1adc77fefc49
boratln <boratulan123@gmail.com>
2024-12-13 16:47:30 +0300

    Mediator SearchBlogsQuery

---
+++ Blogizm.Application/Features/Mediator/Queries/BlogQueries/SearchBlogsQuery.cs
@@ -0,0 +1,20 @@
+﻿using Blogizm.Application.Features.Mediator.Results.BlogResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogizm.Application.Features.Mediator.Queries.BlogQueries
+{
+    public class SearchBlogsQuery:IRequest<List<SearchBlogsQueryResult>>
+    {
+        public string SearchWord {  get; set; }
+
+        public SearchBlogsQuery(string searchWord)
+        {
+            SearchWord = searchWord;
+        }
+    }
+}
