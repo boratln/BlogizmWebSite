c7042b9885fe73828beeece02387a2f4bef5feab
boratln <boratulan123@gmail.com>
2024-12-13 16:46:20 +0300

    Search Action  Added

--- Blogizm.WebUI/Controllers/BlogController.cs
+++ Blogizm.WebUI/Controllers/BlogController.cs
@@ -16,7 +16,40 @@ namespace Blogizm.WebUI.Controllers
         
         public async Task<IActionResult> Index(int id, int page = 1, int itemsPerPage = 5)
         {
-            var client = _httpClientFactory.CreateClient();
+            if (TempData["SearchWord"] != null)
+            {
+                var searchclient = _httpClientFactory.CreateClient();
+                var word = TempData["SearchWord"].ToString();
+                var responseMessage1 = await searchclient.GetAsync("https://localhost:7023/api/Blog/SearchBlogs/" + word);
+                if (responseMessage1.IsSuccessStatusCode)
+                {
+                    var jsonData=await responseMessage1.Content.ReadAsStringAsync();
+                    var values=JsonConvert.DeserializeObject<List<ResultBlogDto>>(jsonData);
+                    var client2 = _httpClientFactory.CreateClient();
+                    var BlogCountResponse = await client2.GetAsync("https://localhost:7023/api/Blog/GetBlogByCategoryIdCount/" + id);
+                    if (BlogCountResponse.IsSuccessStatusCode)
+                    {
+                        var BlogCountjsonData = await BlogCountResponse.Content.ReadAsStringAsync();
+                        var count = JsonConvert.DeserializeObject<ResultBlogCountDto>(BlogCountjsonData);
+                        var pagination = new BlogPaginationDto
+                        {
+                            TotalItems = count.BlogCount,
+                            ItemsPerPage = itemsPerPage,
+                            CurrentPage = page
+                        };
+                        var pagedValues = values
+              .Skip((page - 1) * itemsPerPage)
+              .Take(itemsPerPage)
+              .ToList();
+                        ViewBag.Pagination = pagination;
+                        ViewBag.count = count.BlogCount;
+                        ViewBag.id = id;
+                        return View(pagedValues);
+                    }
+                }
+
+            }
+            var client=_httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:7023/api/Blog/GetBlogByCategoryId/" + id);
             if (responseMessage.IsSuccessStatusCode)
             {
@@ -50,12 +83,18 @@ namespace Blogizm.WebUI.Controllers
         
             return View();
         }
-
+    
         [HttpPost]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string word)
         {
-            //Yorum eklense
-            return RedirectToAction("Index", "Blog");
+
+
+            if (!string.IsNullOrEmpty(word))
+            {
+                TempData["SearchWord"] = word; 
+            }
+
+            return RedirectToAction("Index");
         }
         public async Task<IActionResult> BlogDetail(int id,string param)
         {
